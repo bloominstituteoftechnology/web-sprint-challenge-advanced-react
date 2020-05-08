d3f3590bdb199981d452752ea04ec08cc99c26a8
eriksandviksea <eriksandvikSEA@Gmail.com>
2020-05-08 08:54:12 -0700

    form header render test

--- client/src/components/CheckoutForm.test.js
+++ client/src/components/CheckoutForm.test.js
@@ -4,6 +4,12 @@ import CheckoutForm from "./CheckoutForm";
 
 // Write up the two tests here and make sure they are testing what the title shows
 
-test("form header renders", () => {});
+test("form header renders", () => {
+     const { getByText } = render(<CheckoutForm />)
+
+     const formHeader = getByText(/checkout form/i)
+
+     expect(formHeader).toBeInTheDocument()
+});
 
 test("form shows success message on submit with form details", () => {});
