dee6a5d9da40bd951544bbea781cf04ff1be89b7
eriksandviksea <eriksandvikSEA@Gmail.com>
2020-05-08 09:05:53 -0700

    added check tests for each success message data

--- client/src/components/CheckoutForm.test.js
+++ client/src/components/CheckoutForm.test.js
@@ -11,16 +11,47 @@ test("form header renders", () => {
 });
 
 test("form shows success message on submit with form details", () => {
-     const { getByTestId, getByLabelText } = render (<CheckoutForm />)
+     const { getByTestId, getByLabelText, getByText } = render (<CheckoutForm />)
 
+     const firstName = getByLabelText(/first name:/i)
+     const lastName = getByLabelText(/last name:/i)
+     const address = getByLabelText(/address:/i)
+     const city = getByLabelText(/city:/i)
+     const state = getByLabelText(/state:/i)
+     const zip = getByLabelText(/zip:/i)
      const checkoutButton = getByTestId('checkoutButton')
+
+     expect(firstName).toBeInTheDocument()
+     expect(lastName).toBeInTheDocument()
+     expect(address).toBeInTheDocument()
+     expect(city).toBeInTheDocument()
+     expect(state).toBeInTheDocument()
+     expect(zip).toBeInTheDocument()
+     expect(checkoutButton).toBeInTheDocument()
+
+     fireEvent.change(firstName, { target: { value: 'Willy' } })
+     fireEvent.change(lastName, { target: { value: 'Wonka' } })
+     fireEvent.change(address, { target: { value: '123 Chocolate Factory Ln' } })
+     fireEvent.change(city, { target: { value: 'Industrial' } })
+     fireEvent.change(state, { target: { value: 'England' } })
+     fireEvent.change(zip, { target: { value: '88392810848937297398' } })
+
      fireEvent.click(checkoutButton)
 
      const successMessage = getByTestId('successMessage')
      expect(successMessage).toBeInTheDocument()
 
+     const successMessageName = getByText(/willy wonka/i)
+     const successMessageAddress = getByText(/123 chocolate factory ln/i)
+     const successMessageCity = getByText(/industrial/i)     
+     const successMessageState = getByText(/england/i)
+     const successMessageZip = getByText(/88392810848937297398/i)
 
-
+     expect(successMessageName).toBeInTheDocument()
+     expect(successMessageAddress).toBeInTheDocument()
+     expect(successMessageCity).toBeInTheDocument()
+     expect(successMessageState).toBeInTheDocument()
+     expect(successMessageZip).toBeInTheDocument()
 
 
 });
