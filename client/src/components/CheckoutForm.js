38d74a3329f42415c953b5400860c133cd03c1bd
lex-marie790 <lexi.marie790@gmail.com>
2020-05-15 19:48:54 -0700

    fixed my custom hook, message now pops up!

--- client/src/components/CheckoutForm.js
+++ client/src/components/CheckoutForm.js
@@ -14,8 +14,15 @@ const initialValue = {
 // Build out the logic needed for a form custom hook (see the useForm.js file)
 // and replace the necessary stateful logic from CheckoutForm with the hook
 
-const CheckoutForm = (props) => {
-  const [showSuccessMessage, values, handleChanges, handleSubmit] = useState();
+const CheckoutForm = props => {
+  const [showSuccessMessage, setShowSuccessMessage] = useState(false);
+  const [ values, handleChanges ] = useForm(initialValue);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setShowSuccessMessage(true);
+  };
+
 
   return (
     <>
--- client/src/hooks/useForm.js
+++ client/src/hooks/useForm.js
@@ -1,9 +1,12 @@
 import { useState } from "react"
 
+
+
 // write your custom hook here to control your checkout form
-const useForm = (key, initialValue) => {
+    const useForm = (initialValue) => {
+   
     const [values, setValues] = useState(initialValue);
-    const [showSuccessMessage, setShowSuccessMessage] = useState(false);
+    
 
     const handleChanges = e => {
         setValues({
@@ -11,11 +14,10 @@ const useForm = (key, initialValue) => {
             [e.target.name]: e.target.value,
         });
 
-        const handleSubmit = (e) => {
-            e.preventDefault();
-            setShowSuccessMessage(true);
-          };
-    }
+    };
+
+          return [handleChanges, values]
+    
 };
 
 export default useForm;
