467517494baf9eeb40df31c5695cb9e2f12f6af8
Brian Clayton <finalb@projectfinalboss.com>
2020-05-15 23:23:35 -0400

    stateful logic complete

--- client/src/hooks/useForm.js
+++ client/src/hooks/useForm.js
@@ -1,1 +1,25 @@
 // write your custom hook here to control your checkout form
+import {useState} from 'react';
+
+
+ const useForm = initialState => {
+
+    const [values, setValues] = useState(initialState);
+    
+    const handleChanges = (e) => {
+        setValues({ ...values, [e.target.name]: e.target.value });
+      };
+
+
+        const clearForm = () => {
+
+            setValues(initialState);
+        }
+
+        return [values, handleChanges, clearForm];
+
+    }
+
+
+
+export default useForm;
