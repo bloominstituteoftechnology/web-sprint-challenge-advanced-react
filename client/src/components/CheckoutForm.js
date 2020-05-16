48d5548e4cc3eb69f03432cca9b2c622aefb5224
Matthew Hidalgo <mhidalgo83@outlook.com>
2020-05-16 11:42:06 -0700

    mvp complete

--- client/src/components/CheckoutForm.js
+++ client/src/components/CheckoutForm.js
@@ -16,20 +16,13 @@ const initialValue = {
 
 const CheckoutForm = (props) => {
   const [showSuccessMessage, setShowSuccessMessage] = useState(false);
-  // const [values, setValues] = useState(initialValue);
 
-  // const handleChanges = (e) => {
-  //   setValues({ ...values, [e.target.name]: e.target.value });
-  // };
-
-  // const handleSubmit = (e) => {
-  //   e.preventDefault();
-  //   setShowSuccessMessage(true);
-  // };
-
-  const [values, handleChanges, handleSubmit, handleClearForm] = useForm(initialValue, () =>
-    setShowSuccessMessage(true)
-  );
+  const [
+    values,
+    handleChanges,
+    handleSubmit,
+    handleClearForm,
+  ] = useForm(initialValue, () => setShowSuccessMessage(true));
 
   console.log(values);
   return (
--- client/src/components/PlantList.js
+++ client/src/components/PlantList.js
@@ -4,18 +4,25 @@ import axios from "axios";
 export default class PlantList extends Component {
   state = {
     plants: [],
+    search: "",
   };
   // add state with a property called "plants" - initialize as an empty array
 
   // when the component mounts:
   //   - fetch data from the server endpoint - http://localhost:3333/plants
   //   - set the returned plants array to this.state.plants
+  handleChange = (e) => {
+    this.setState({ search: e.target.value });
+    
+  };
+
+  componentDidUpdate() {}
 
   componentDidMount() {
     axios.get("http://localhost:3333/plants").then((res) => {
       console.log(res.data.plantsData);
       const plants = res.data.plantsData;
-      this.setState({plants: plants})
+      this.setState({ plants: plants });
     });
   }
 
@@ -23,6 +30,12 @@ export default class PlantList extends Component {
   render() {
     return (
       <main className="plant-list">
+        <input
+          id="search"
+          name="search"
+          value={this.state.search}
+          onChange={this.handleChange}
+        />
         {this.state?.plants?.map((plant) => (
           <div className="plant-card" key={plant.id}>
             <img className="plant-image" src={plant.img} alt={plant.name} />
