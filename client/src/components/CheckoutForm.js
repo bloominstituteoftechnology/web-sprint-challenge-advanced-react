8bb0e2a88c0fb8cc7cbd391e4013fc3a6f7ea83f
Matthew Hidalgo <mhidalgo83@outlook.com>
2020-05-16 10:25:30 -0700

    handlechanges working properly

--- client/src/components/CheckoutForm.js
+++ client/src/components/CheckoutForm.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { useForm } from "../hooks/useForm";
 
 const initialValue = {
   firstName: "",
@@ -15,17 +16,19 @@ const initialValue = {
 
 const CheckoutForm = (props) => {
   const [showSuccessMessage, setShowSuccessMessage] = useState(false);
-  const [values, setValues] = useState(initialValue);
+  // const [values, setValues] = useState(initialValue);
 
-  const handleChanges = (e) => {
-    setValues({ ...values, [e.target.name]: e.target.value });
-  };
+  // const handleChanges = (e) => {
+  //   setValues({ ...values, [e.target.name]: e.target.value });
+  // };
 
   const handleSubmit = (e) => {
     e.preventDefault();
     setShowSuccessMessage(true);
   };
 
+  const [values, handleChanges] = useForm(initialValue);
+  console.log(values);
   return (
     <>
       <form onSubmit={handleSubmit}>
@@ -33,6 +36,7 @@ const CheckoutForm = (props) => {
         <label>
           First Name:
           <input
+            id="name"
             name="firstName"
             value={values.firstName}
             onChange={handleChanges}
