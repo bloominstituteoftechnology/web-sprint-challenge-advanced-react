beb5a55250d9b433bf5b5e65bed6f540cd8cc70a
Brian Clayton <finalb@projectfinalboss.com>
2020-05-15 23:58:05 -0400

    Update CheckoutForm.test.js

--- client/src/components/CheckoutForm.test.js
+++ client/src/components/CheckoutForm.test.js
@@ -1,9 +1,38 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, getByLabelText, fireEvent } from "@testing-library/react";
 import CheckoutForm from "./CheckoutForm";
 
 // Write up the two tests here and make sure they are testing what the title shows
 
-test("form header renders", () => {});
+test("form header renders", () => {
 
-test("form shows success message on submit with form details", () => {});
+    //Arrange
+    const container = render(<CheckoutForm/>);
+
+    const header = container.getByText("Checkout Form")
+
+    expect(header).toBeInTheDocument();
+
+
+});
+
+test("form shows success message on submit with form details", () => {
+
+    const {getByLabelText, getByText} = render(<CheckoutForm />);
+
+    const firstInput = getByLabelText();
+    const lastInput = getByLabelText();
+    const addressInput = getByLabelText();
+    const cityInput = getByLabelText();
+    const stateInput = getByLabelText();
+    const zipInput = getByLabelText();
+
+    const button = getByText();
+
+    fireEvent.click(getByTex(/Checkout/1));
+    expect()
+
+
+
+
+});
