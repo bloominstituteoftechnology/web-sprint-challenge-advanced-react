e80a04438a7e1307d4d8b936e2b526bfcf4c0a2f
Dessa Goodlett <53787065+dmhabh1992@users.noreply.github.com>
2020-05-17 22:49:04 -0500

    tried running tests again only one runs

--- client/src/components/CheckoutForm.test.js
+++ client/src/components/CheckoutForm.test.js
@@ -1,24 +1,24 @@
 import React from "react";
 import { render } from "@testing-library/react";
 import Form from "./CheckoutForm";
-import TestRenderer from 'react-test-renderer';
+ import TestRenderer from 'react-test-renderer';
 
-// Write up the two tests here and make sure they are testing what the title shows
+ //Write up the two tests here and make sure they are testing what the title shows
 
-test("form header renders", async () => {};
-//     //arrange
-//     const {findByText} = render(<header/>);
-//     //act
-//     const header = findByText(/checkout/i);
-//     // assert
-//     expect(header).toBeInTheDocument();
-// });
+test("form header renders", async () => {
+    arrange
+    const {findByText} = render(<header/>);
+    act
+    const header = findByText(/checkout/i);
+     assert
+    expect(header).toBeInTheDocument();
+});
 
-test("form shows success message on submit with form details", async () => {};
-//     //arrange
-//     const {findByText} = render(<submitted/>);
-//     //act
-//     const submitted = findByText(/woo-hoo/i);
-//     //assert
-//     expect(submitted).toBeInTheDocument();
-// });
+test("form shows success message on submit with form details", async () => {
+     arrange
+     const {findByText} = render(<submitted/>);
+     act
+     const submitted = findByText(/woo-hoo/i);
+     assert
+     expect(submitted).toBeInTheDocument();
+ });
