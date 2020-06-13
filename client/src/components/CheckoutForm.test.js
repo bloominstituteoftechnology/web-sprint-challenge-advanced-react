a043d9a84afd892fc1e4dd0e956fea6a2b7dff1b
William Webb <webbdevvue@gmail.com>
2020-06-12 22:00:14 -0400

    still working on the last test

--- client/src/components/CheckoutForm.test.js
+++ client/src/components/CheckoutForm.test.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, fireEvent } from "@testing-library/react";
+import '@testing-library/jest-dom/extend-expect';
 import CheckoutForm from "./CheckoutForm";
 
 // Write up the two tests here and make sure they are testing what the title shows
@@ -15,8 +16,5 @@ test("form header renders", () => {
 
 test("form shows success message on submit with form details", () => {
     const { getByTestId } = render(<CheckoutForm />);
-    fireEvent.change(getByTestId(/successmessage/i), {
-
-    })
-    
+    getByTestId(/successmessage/i);
 });
