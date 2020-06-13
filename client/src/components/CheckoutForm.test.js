00c1d6f645efb3b562cab0cb48258b12a88f6b7b
William Webb <webbdevvue@gmail.com>
2020-06-12 20:44:20 -0400

    working on the test file

--- client/src/components/CheckoutForm.test.js
+++ client/src/components/CheckoutForm.test.js
@@ -4,6 +4,13 @@ import CheckoutForm from "./CheckoutForm";
 
 // Write up the two tests here and make sure they are testing what the title shows
 
-test("form header renders", () => {});
+test("form header renders", () => {
+    //Arrange
+    const { getByText } = render(<CheckoutForm />)
+    //Act
+    getByText(/react plants/i)
+    //Assert
+});
+    
 
 test("form shows success message on submit with form details", () => {});
