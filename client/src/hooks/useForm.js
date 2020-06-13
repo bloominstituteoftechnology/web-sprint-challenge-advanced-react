7c3b70b7b02e9e7cd712f12b979bf0877034cd39
William Webb <webbdevvue@gmail.com>
2020-06-12 20:11:51 -0400

    added useForm custom hook, adds inputs data successfully

--- client/src/hooks/useForm.js
+++ client/src/hooks/useForm.js
@@ -1,1 +1,18 @@
 // write your custom hook here to control your checkout form
+import React, {useState} from 'react';
+
+export const useForm = (key, initialValue, cb) => {
+    const [showSuccessMessage, setShowSuccessMessage] = useState(false);
+    const [values, setValues] = useState(initialValue);
+
+    const handleChanges = (e) => {
+        setValues({ ...values, [e.target.name]: e.target.value });
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setShowSuccessMessage(true);
+    };
+
+    return [showSuccessMessage, values, handleChanges, handleSubmit];
+}
