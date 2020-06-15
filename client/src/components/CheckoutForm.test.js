949040c6ccc20c3d83141bc53866809de5e21c58
Tashby <tashby2714@gmail.com>
2020-06-14 20:21:15 -0400

    removed async

--- client/src/components/CheckoutForm.test.js
+++ client/src/components/CheckoutForm.test.js
@@ -4,13 +4,13 @@ import CheckoutForm from "./CheckoutForm";
 
 // Write up the two tests here and make sure they are testing what the title shows
 
-test("form header renders", async () => {
+test("form header renders" () => {
     const { getByText } = render(<CheckoutForm/>);
 
     getByText(/Checkout Form/i)
 });
 
-test("form shows success message on submit with form details", async () => {
+test("form shows success message on submit with form details" () => {
     const { getByText, getByTestId } = render(<CheckoutForm />)
 
     const button = getByTestId(/CheckoutID/i)
