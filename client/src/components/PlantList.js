0a936f2b5524a40fd7c520c489d05c5f2bc6e603
Tashby <tashby2714@gmail.com>
2020-06-14 20:14:42 -0400

    mvp

--- client/src/components/PlantList.js
+++ client/src/components/PlantList.js
@@ -10,7 +10,6 @@ export default class PlantList extends Component {
   //   - fetch data from the server endpoint - http://localhost:3333/plants
   //   - set the returned plants array to this.state.plants
   componentDidMount() {
-    console.log("ComponentDidMount running");
     axios
       .get("http://localhost:3333/plants")
       .then(res => {
--- client/src/hooks/useForm.js
+++ client/src/hooks/useForm.js
@@ -1,1 +1,24 @@
 // write your custom hook here to control your checkout form
+import { useLocalStorage } from "./useLocalStorage";
+
+export const useForm = (initialValues, key) => { 
+    //set
+    const [values, setValues] = useLocalStorage(initialValues, key);
+    const [showSuccessMessage, setShowSuccessMessage] = useLocalStorage('Success', false)
+
+    //changehandle
+    const handleChanges = e => {
+        setValues({
+            ...values,
+            [e.target.name]: e.target.value
+        });
+    };
+    //submit
+    const handleSubmit = e => {
+        e.preventDefault();
+        setShowSuccessMessage(true);
+        };
+        
+    //return
+    return [values, handleChanges, handleSubmit, showSuccessMessage];
+}
