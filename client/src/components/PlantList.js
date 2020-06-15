db38e0893c92f159e61b53a74c94e450077ffc43
dipeshkoirala <dipeshkoirala@gmail.com>
2020-06-14 20:09:51 -0400

    plantList

--- client/src/components/PlantList.js
+++ client/src/components/PlantList.js
@@ -1,18 +1,55 @@
 import React, { Component } from "react";
+
 import axios from "axios";
 
 export default class PlantList extends Component {
-  // add state with a property called "plants" - initialize as an empty array
+  // add state with a property called "plants" -
+  //  initialize as an empty plantsay
 
+  state = {
+    plants: [],
+  };
+
+  componentDidMount() {
+    console.log("componentDidMount running");
+    axios
+      .get("http://localhost:3333/plants")
+      .then((res) => {
+        console.log(res.data.plantsData);
+        this.setState({ plants: res.data.plantsData });
+      })
+      .catch((err) => console.log(err));
+  }
+
+  handleChanges = (e) => {
+    e.preventDefault();
+    // console.log(e.target.name, e.target.value);
+    this.setState({ ...this.state, user: e.target.value });
+  };
+
+  getUser = (e) => {
+    e.preventDefault();
+    axios
+
+      .get(`https://api.github.com/users/${this.state.user}`)
+
+      .then((res) => {
+        this.setState({ ...this.state, plants: res.data });
+        // console.log(res);
+      })
+      .catch((err) => console.log(err));
+
+    // console.log("dk->" + this.state);
+  };
   // when the component mounts:
   //   - fetch data from the server endpoint - http://localhost:3333/plants
-  //   - set the returned plants array to this.state.plants
+  //   - set the returned plants plantsay to this.state.plants
 
   /*********  DON'T CHANGE ANYTHING IN THE RENDER FUNCTION *********/
   render() {
     return (
       <main className="plant-list">
-        {this.state?.plants?.map((plant) => (
+        {this.state.plants.map((plant) => (
           <div className="plant-card" key={plant.id}>
             <img className="plant-image" src={plant.img} alt={plant.name} />
             <div className="plant-details">
--- client/src/hooks/useForm.js
+++ client/src/hooks/useForm.js
@@ -1,1 +1,16 @@
 // write your custom hook here to control your checkout form
+import { checkoutForm } from "../components/CheckoutForm";
+import { useEffect } from "react";
+
+export const useDarkMode = (key, initialValue) => {
+  const [values, setValues] = useLocalStorage();
+
+  useEffect(() => {
+    if (values === true) {
+      document.body.classList.add("dark-mode");
+    } else {
+      document.body.classList.remove("dark-mode");
+    }
+  }, [values]);
+  return [values, setValues];
+};
