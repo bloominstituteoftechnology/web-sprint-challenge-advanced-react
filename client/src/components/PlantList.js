bacae56169661ca75e7712b93b8049fd5320807b
Benjamin S Ogzewalla <saigeogzewalla@gmail.com>
2020-06-14 20:15:32 -0400

    Api Call and Custom Hook Done

--- client/src/components/PlantList.js
+++ client/src/components/PlantList.js
@@ -1,9 +1,28 @@
 import React, { Component } from "react";
 import axios from "axios";
 
+
 export default class PlantList extends Component {
   // add state with a property called "plants" - initialize as an empty array
+  state = {
+    plants: [],
+  }
 
+    componentDidMount() {
+      axios
+        .get("http://localhost:3333/plants")
+        .then((res) => {
+          console.log(res.data.plantsData);
+          this.setState({
+            plants: res.data.plantsData,
+          });
+        })
+  
+        .catch((err) => {
+          console.log(err)
+        });
+    
+  }
   // when the component mounts:
   //   - fetch data from the server endpoint - http://localhost:3333/plants
   //   - set the returned plants array to this.state.plants
--- client/src/hooks/useForm.js
+++ client/src/hooks/useForm.js
@@ -1,1 +1,37 @@
 // write your custom hook here to control your checkout form
+
+import { useState } from "react";
+
+const useForm = (cb) => {
+
+    const initialValue = {
+        firstName: "",
+        lastName: "",
+        address: "",
+        city: "",
+        state: "",
+        zip: "",
+      };
+
+    const [showSuccessMessage, setShowSuccessMessage] = useState(false);
+    const [values, setValues] = useState(initialValue);
+
+    const handleChanges = (e) => {
+        setValues({ ...values, [e.target.name]: e.target.value });
+      };
+    
+      const handleSubmit = (e) => {
+        e.preventDefault();
+        cb();
+      };
+
+      return {
+        handleChanges,
+        handleSubmit,
+        values,
+        showSuccessMessage
+      }
+
+}
+
+export default useForm;
