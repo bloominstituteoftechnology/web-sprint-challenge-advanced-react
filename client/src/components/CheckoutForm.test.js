0617aaab4be5568beacd51358c396a62dedddf9e
dipeshkoirala <dipeshkoirala@gmail.com>
2020-06-19 17:34:41 -0400

    testing the header render

--- client/src/components/CheckoutForm.test.js
+++ client/src/components/CheckoutForm.test.js
@@ -1,9 +1,14 @@
 import React from "react";
 import { render } from "@testing-library/react";
 import CheckoutForm from "./CheckoutForm";
+import "@testing-library/jest-dom/extend-expect";
 
 // Write up the two tests here and make sure they are testing what the title shows
 
-test("form header renders", () => {});
+test("form header renders", () => {
+  const { getByText } = render(<CheckoutForm />);
+
+  getByText(/Checkout form/i);
+});
 
 test("form shows success message on submit with form details", () => {});
