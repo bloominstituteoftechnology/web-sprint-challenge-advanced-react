2d19d5ba30e0849ba9f26bd3b5261163ac61ff0f
Shaun Lee <shaundlee.0103@gmail.com>
2020-07-10 11:43:13 -0400

    Installed npm dependencies and initialized app state

--- client/src/components/PlantList.js
+++ client/src/components/PlantList.js
@@ -3,6 +3,9 @@ import axios from "axios";
 
 export default class PlantList extends Component {
   // add state with a property called "plants" - initialize as an empty array
+  state = {
+    plants: []
+  }
 
   // when the component mounts:
   //   - fetch data from the server endpoint - http://localhost:3333/plants
