793a6248dcac9dd7efaf6ecc084456fe0799bd2b
Julian Leon <jleon1992@gmail.com>
2020-07-10 12:59:27 -0400

    MVP met

--- client/src/components/CheckoutForm.js
+++ client/src/components/CheckoutForm.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import {useForm} from '../hooks/useForm'
 
 const initialValue = {
   firstName: "",
@@ -15,11 +16,9 @@ const initialValue = {
 
 const CheckoutForm = (props) => {
   const [showSuccessMessage, setShowSuccessMessage] = useState(false);
-  const [values, setValues] = useState(initialValue);
+  const [values, handleChanges] = useForm(initialValue)
 
-  const handleChanges = (e) => {
-    setValues({ ...values, [e.target.name]: e.target.value });
-  };
+
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -66,7 +65,7 @@ const CheckoutForm = (props) => {
           Zip:
           <input name="zip" value={values.zip} onChange={handleChanges} />
         </label>
-        <button>Checkout</button>
+        <button placeholder='checkout'>Checkout</button>
       </form>
 
       {showSuccessMessage && (
--- client/src/components/CheckoutForm.test.js
+++ client/src/components/CheckoutForm.test.js
@@ -1,9 +1,52 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import CheckoutForm from "./CheckoutForm";
 
 // Write up the two tests here and make sure they are testing what the title shows
 
-test("form header renders", () => {});
+test("form header renders", () => {
+    render(<CheckoutForm/>)
+    const header = screen.getByText(/checkout form/i)
+    expect(header).toBeInTheDocument
+});
+test("success message renders details correctly", () => {
+    render(<CheckoutForm/>)
+    const firstNameInput = screen.getByLabelText(/first name/i)
+    fireEvent.change(firstNameInput, {target: {value: 'Thomas'}})
+    const checkoutButton = screen.getByPlaceholderText(/checkout/i)
+    fireEvent.click(checkoutButton)
 
-test("form shows success message on submit with form details", () => {});
+    const message = screen.getByText(/thomas/i)
+    console.log(message)
+
+
+})
+
+test("form shows success message on submit with form details", () => {
+    render(<CheckoutForm/>)
+
+    const firstNameInput = screen.getByLabelText(/first name/i)
+    const lastNameInput = screen.getByLabelText(/last name/i)
+    const addressInput = screen.getByLabelText(/last name/i)
+    const cityInput = screen.getByLabelText(/city/i)
+    const stateInput = screen.getByLabelText(/state/i)
+    const zipInput = screen.getByLabelText(/zip/i)
+    const checkoutButton = screen.getByPlaceholderText(/checkout/i)
+
+    fireEvent.change(firstNameInput, {target: {value: 'Thomas'}})
+    fireEvent.change(lastNameInput, {target: {value: 'BeckingHam'}})
+    fireEvent.change(addressInput, {target: {value: '101 1st street'}})
+    fireEvent.change(cityInput, {target: {value: 'Ethiopia'}})
+    fireEvent.change(stateInput, {target: {value: 'Queens'}})
+    fireEvent.change(zipInput, {target: {value: 12321}})
+ 
+    
+    
+    fireEvent.click(checkoutButton)
+    
+    const successMessage = screen.findByText(/you have ordered/i)
+    expect(successMessage).toBeInTheDocument 
+    console.log(successMessage)
+
+
+});
