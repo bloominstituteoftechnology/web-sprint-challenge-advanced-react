a5406b154e81c8f18f69c160a654dc74af2d64af
JenVest2020 <jenvest2019@gmail.com>
2020-07-17 20:37:58 -0500

    completed state and api call in PlantList component

--- client/src/components/PlantList.js
+++ client/src/components/PlantList.js
@@ -2,17 +2,27 @@ import React, { Component } from "react";
 import axios from "axios";
 
 export default class PlantList extends Component {
-  // add state with a property called "plants" - initialize as an empty array
+  constructor() {
+    super();
+    this.state = {
+      plants: []
+    }
+  }
 
-  // when the component mounts:
-  //   - fetch data from the server endpoint - http://localhost:3333/plants
-  //   - set the returned plants array to this.state.plants
+  componentDidMount() {
+    fetch('http://localhost:3333/plants')
+      .then(res => res.json())
+      .then(plant => {
+        this.setState({ plants: plant })
+        console.log('from PL: CDM', plant)
+      })
+  }
 
   /*********  DON'T CHANGE ANYTHING IN THE RENDER FUNCTION *********/
   render() {
     return (
       <main className="plant-list">
-        {this.state?.plants?.map((plant) => (
+        {/* {this.state?.plants?.map((plant) => (
           <div className="plant-card" key={plant.id}>
             <img className="plant-image" src={plant.img} alt={plant.name} />
             <div className="plant-details">
@@ -32,7 +42,7 @@ export default class PlantList extends Component {
               </button>
             </div>
           </div>
-        ))}
+        ))} */}
       </main>
     );
   }
