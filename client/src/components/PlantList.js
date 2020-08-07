fb9150a60c92a56c8959e3c744f25db11863fd89
Logan Metzger <loganmetzger@MacBook-Pro.local>
2020-08-07 11:20:23 -0400

    created constructor function with super and empt array for plants as state. made axios call and set res to state as plants

--- client/src/components/PlantList.js
+++ client/src/components/PlantList.js
@@ -2,6 +2,24 @@ import React, { Component } from "react";
 import axios from "axios";
 
 export default class PlantList extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      plants: []
+    };
+  }
+
+  componentDidMount() {
+    axios.get('http://localhost:3333/plants')
+      .then(res => {
+        console.log(res)
+        this.setState({ plants: res.data.plantsData });
+      })
+      .catch(err => console.log(err))
+  }
+
+
   // add state with a property called "plants" - initialize as an empty array
 
   // when the component mounts:
