a7ffab4c2d77af43973b7d5b93e17f2a5762bcc5
Logan Metzger <loganmetzger@MacBook-Pro.local>
2020-08-07 11:24:47 -0400

    custom hook fleshed out, hoping it works decently well

--- client/src/hooks/useForm.js
+++ client/src/hooks/useForm.js
@@ -1,1 +1,15 @@
 // write your custom hook here to control your checkout form
+import { useState } from 'react'
+
+export function useForm(initialForm) {
+    const [form, setForm] = useState(initialForm);
+
+    const handleChanges = e => {
+        setForm({
+            ...form,
+            [e.target.name]: e.target.value
+        });
+    }
+
+    return [form, handleChanges]
+}
