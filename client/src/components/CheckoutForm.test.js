53e2eaab1a61a18d2dafc9e6d999b9c664616f60
Logan Metzger <loganmetzger@MacBook-Pro.local>
2020-08-07 11:59:58 -0400

    found problem, imported user event to get things to work

--- client/src/components/CheckoutForm.test.js
+++ client/src/components/CheckoutForm.test.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { render, fireEvent } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import CheckoutForm from "./CheckoutForm";
 
 // Write up the two tests here and make sure they are testing what the title shows
