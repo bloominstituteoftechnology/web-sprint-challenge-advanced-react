700b76c90e853330ff275b09e0baa3896b8ce8f8
noah niesen <noah@noahs-MacBook-Pro.local>
2020-08-14 18:21:02 -0500

    added state with property called 'plants' which is initialized as an epty array. Added an axios call that allows the component to mount and set the received array to this.state.plants

--- client/src/components/PlantList.js
+++ client/src/components/PlantList.js
@@ -3,10 +3,27 @@ import axios from "axios";
 
 export default class PlantList extends Component {
   // add state with a property called "plants" - initialize as an empty array
+  constructor() {
+    super();
+    this.state = {
+      plants: []
+    }
+  }
 
   // when the component mounts:
   //   - fetch data from the server endpoint - http://localhost:3333/plants
   //   - set the returned plants array to this.state.plants
+  componentDidMount() {
+    axios
+      .get('http://localhost:3333/plants')
+      .then((res) => {
+        const plantsArray = res.data.plantsData;
+        this.setState({ plants: plantsArray });
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+  }
 
   /*********  DON'T CHANGE ANYTHING IN THE RENDER FUNCTION *********/
   render() {
