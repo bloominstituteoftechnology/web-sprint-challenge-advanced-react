b0e98bb28f9546ad29d7a10fe888cc78cdcb9558
Bruno Paula <brunopaula@dryserv.com>
2020-08-14 19:51:16 -0400

    Added a Custom Hook

--- client/src/components/CheckoutForm.test.js
+++ client/src/components/CheckoutForm.test.js
@@ -1,9 +1,13 @@
-import React from "react";
-import { render } from "@testing-library/react";
-import CheckoutForm from "./CheckoutForm";
+import React from 'react'
+import { render } from '@testing-library/react'
+import CheckoutForm from './CheckoutForm'
 
 // Write up the two tests here and make sure they are testing what the title shows
 
-test("form header renders", () => {});
+test('CheckOut Page renders withour Crashing', () => {
+  render(<CheckoutForm />)
+})
 
-test("form shows success message on submit with form details", () => {});
+test('form header renders', () => {})
+
+test('form shows success message on submit with form details', () => {})
