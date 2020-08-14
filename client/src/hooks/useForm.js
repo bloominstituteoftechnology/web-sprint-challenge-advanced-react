0241b4c16177b3d464c5b8df0e63beddb363fece
Bruno Paula <brunopaula@dryserv.com>
2020-08-14 19:51:59 -0400

    Created a Custom User Form Hook

--- client/src/hooks/useForm.js
+++ client/src/hooks/useForm.js
@@ -1,1 +1,28 @@
-// write your custom hook here to control your checkout form
+import { useState } from 'react'
+
+const useForm = (initalValue) => {
+  const [showSuccessMessage, setShowSuccessMessage] = useState(false)
+  const [values, setValues] = useState(initalValue)
+
+  const handleSubmit = (event) => {
+    if (event) event.preventDefault()
+    setShowSuccessMessage(true)
+  }
+
+  const handleChanges = (event) => {
+    event.persist()
+    setValues((values) => ({
+      ...values,
+      [event.target.name]: event.target.value,
+    }))
+  }
+
+  return {
+    values,
+    handleChanges,
+    handleSubmit,
+    showSuccessMessage,
+  }
+}
+
+export default useForm
