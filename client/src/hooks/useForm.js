0b70c6845693de3ba7160a747201af5e580bb0da
River Smith <rivyyriv@rivers-mbp.lan>
2020-08-14 18:48:05 -0600

    API Connected

--- client/src/hooks/useForm.js
+++ client/src/hooks/useForm.js
@@ -1,1 +1,12 @@
 // write your custom hook here to control your checkout form
+import React, { useState } from "react";
+
+export const useForm = initialValue => {
+    const [value, setValue] = useState(initialValue);
+    const handleChanges = updatedValue => {
+      setValue(updatedValue);
+    };
+    return [value, setValue, handleChanges];
+  };
+
+  export default useForm;
