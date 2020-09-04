e5536ef962cfb69c9b5c1c7fb336ded0ad962012
mhmulford0 <mhmulford0@gmail.com>
2020-09-04 10:44:40 -0500

    added test for submit button click

--- client/src/components/CheckoutForm.test.js
+++ client/src/components/CheckoutForm.test.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import CheckoutForm from "./CheckoutForm";
 
 // Write up the two tests here and make sure they are testing what the title shows
@@ -10,4 +10,8 @@ test("form header renders", () => {
   const checkoutHeaderText = screen.getByText('Checkout Form')
 });
 
-test("form shows success message on submit with form details", () => { });
+test("form shows success message on submit with form details", () => {
+  render(<CheckoutForm />)
+  const submit = screen.queryByRole('button')
+  fireEvent.click(submit)
+});
