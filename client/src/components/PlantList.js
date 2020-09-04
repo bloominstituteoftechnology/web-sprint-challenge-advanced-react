1c880891d0a37ebefc349a92e33adcb2cafface5
mhmulford0 <mhmulford0@gmail.com>
2020-09-04 10:15:36 -0500

    state added and data recieved from api

--- client/src/components/PlantList.js
+++ client/src/components/PlantList.js
@@ -9,10 +9,31 @@ export default class PlantList extends Component {
   //   - set the returned plants array to this.state.plants
 
   /*********  DON'T CHANGE ANYTHING IN THE RENDER FUNCTION *********/
+
+  constructor() {
+    super();
+    this.state = {
+      plants: []
+    }
+  }
+
+  componentDidMount() {
+    axios.get("http://localhost:3333/plants")
+      .then(res => {
+        this.setState({
+          plants: res.data.plantsData,
+        })
+
+      })
+  }
+
+
+
   render() {
     return (
+
       <main className="plant-list">
-        {this.state?.plants?.map((plant) => (
+        {this.state.plants.map((plant) => (
           <div className="plant-card" key={plant.id}>
             <img className="plant-image" src={plant.img} alt={plant.name} />
             <div className="plant-details">
