66eab82b9ba5933225026d1bf5570ce3d0a5f81e
mhmulford0 <mhmulford0@gmail.com>
2020-09-04 10:19:36 -0500

    initial logic added to custom hook

--- client/src/components/CheckoutForm.js
+++ client/src/components/CheckoutForm.js
@@ -14,17 +14,6 @@ const initialValue = {
 // and replace the necessary stateful logic from CheckoutForm with the hook
 
 const CheckoutForm = (props) => {
-  const [showSuccessMessage, setShowSuccessMessage] = useState(false);
-  const [values, setValues] = useState(initialValue);
-
-  const handleChanges = (e) => {
-    setValues({ ...values, [e.target.name]: e.target.value });
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    setShowSuccessMessage(true);
-  };
 
   return (
     <>
