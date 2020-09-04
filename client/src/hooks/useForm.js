6a02ce553c51c2decce0ff81c06d3949a78c1505
EmilyA <emilyadams.3d@gmail.com>
2020-09-04 08:57:24 -0700

    Got the custom hook working

--- client/src/hooks/useForm.js
+++ client/src/hooks/useForm.js
@@ -1,1 +1,7 @@
 // write your custom hook here to control your checkout form
+import { useLocalStorage } from "./useLocalStorage";
+
+export const useForm = (key, initialValue) => {
+  const [values, setValues] = useLocalStorage(key, initialValue);
+  return [values, setValues];
+};
