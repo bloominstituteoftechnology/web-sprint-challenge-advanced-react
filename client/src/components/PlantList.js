9bd45fa2185cda63bf403ceacbc87090b14b72d1
EmilyA <emilyadams.3d@gmail.com>
2020-09-04 08:35:49 -0700

    Fixed plants page

--- client/src/components/PlantList.js
+++ client/src/components/PlantList.js
@@ -1,12 +1,28 @@
-import React, { Component } from "react";
+import React, { Component, useState } from "react";
 import axios from "axios";
 
 export default class PlantList extends Component {
   // add state with a property called "plants" - initialize as an empty array
-
+  constructor() {
+    super();
+    this.state = {
+      plants: [],
+    };
+  }
   // when the component mounts:
   //   - fetch data from the server endpoint - http://localhost:3333/plants
   //   - set the returned plants array to this.state.plants
+  componentDidMount() {
+    axios
+      .get("http://localhost:3333/plants")
+      .then((response) => {
+        console.log(response.data);
+        this.setState({
+          plants: response.data.plantsData,
+        });
+      })
+      .catch((error) => console.log(error));
+  }
 
   /*********  DON'T CHANGE ANYTHING IN THE RENDER FUNCTION *********/
   render() {
