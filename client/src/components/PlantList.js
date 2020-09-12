4f8d96dcdbb5336f005c029a495b77959c912953
Shenandoah Veele <64814835+Jamakura@users.noreply.github.com>
2020-09-11 21:47:35 -0400

    plantList.js class component done

--- client/src/components/PlantList.js
+++ client/src/components/PlantList.js
@@ -3,6 +3,17 @@ import axios from "axios";
 
 export default class PlantList extends Component {
   // add state with a property called "plants" - initialize as an empty array
+  constructor() {
+    super();
+    this.state = { plants: [] };
+  }
+
+  componentDidMount() {
+    axios.get(`http://localhost:3333/plants`).then((res) => {
+      this.setState({ plants: this.state.plants.concat(res.data.plantsData) });
+      console.log("SV, PlantList.CDM: worked", res);
+    });
+  }
 
   // when the component mounts:
   //   - fetch data from the server endpoint - http://localhost:3333/plants
