a9d96f2e1cbfb9dca624823da921926f152e7f66
Simone Ballard <simone.a.ballard@gmail.com>
2020-10-02 11:32:27 -0600

    test 1 changed

--- client/src/components/CheckoutForm.test.js
+++ client/src/components/CheckoutForm.test.js
@@ -5,9 +5,8 @@ import CheckoutForm from "./CheckoutForm";
 // Write up the two tests here and make sure they are testing what the title shows
 
 test("form header renders", () => {
-    const { getByText } = render();
-    const headerElement = getByText(/Checkout Form/i);
-    expect(headerElement).toBeInTheDocument();
+    const header = screen.getByText(/Checkout Form/i);
+    expect(header).toBeTruthy();
 });
 
 test("form shows success message on submit with form details", () => {
