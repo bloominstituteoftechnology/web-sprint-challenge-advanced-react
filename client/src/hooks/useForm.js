c99a67500db32a1a3684b78d8aa43000fe119bb6
Simone Ballard <simone.a.ballard@gmail.com>
2020-10-02 10:06:18 -0600

    useForm initialized

--- client/src/hooks/useForm.js
+++ client/src/hooks/useForm.js
@@ -1,1 +1,24 @@
 // write your custom hook here to control your checkout form
+
+import { useState } from "react";
+
+export const useForm = (props) => {
+    const [showSuccessMessage, setShowSuccessMessage] = useState(false);
+    const [values, setValues] = useState(initialValue);
+
+    
+  const handleChanges = (e) => {
+    setValues({ 
+        ...values, 
+        [e.target.name]: e.target.value 
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setShowSuccessMessage(true);
+  };
+
+return [showSuccessMessage, values, handleSubmit, handleChanges];
+
+}
