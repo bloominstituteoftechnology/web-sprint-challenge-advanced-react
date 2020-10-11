e47bbbc831d7718e8df79a578534d3fab4628440
Mohamed essamaali <mohamed.essamaali@gmail.com>
2020-10-10 20:28:17 -0400

    mvp completed

--- client/src/components/PlantList.js
+++ client/src/components/PlantList.js
@@ -3,13 +3,33 @@ import axios from "axios";
 
 export default class PlantList extends Component {
   // add state with a property called "plants" - initialize as an empty array
+  constructor(){
+    super();
+    this.state={plants:[]}
 
+  }
+ 
   // when the component mounts:
   //   - fetch data from the server endpoint - http://localhost:3333/plants
   //   - set the returned plants array to this.state.plants
 
+  componentDidMount(){
+   axios
+      .get('http://localhost:3333/plants')
+      .then(res=>{
+        console.log('axios response ',res.data.plantsData)
+        this.setState({...this.state.plants,plants:res.data.plantsData})
+       
+      })
+      .catch(err=>{
+        console.log('errors',err)
+      })
+    
+  }
+ 
   /*********  DON'T CHANGE ANYTHING IN THE RENDER FUNCTION *********/
   render() {
+   
     return (
       <main className="plant-list">
         {this.state?.plants?.map((plant) => (
--- client/src/hooks/useForm.js
+++ client/src/hooks/useForm.js
@@ -1,1 +1,24 @@
 // write your custom hook here to control your checkout form
+
+import { useState } from "react"
+
+export function useForm(key,initialValues){
+
+
+    const[values,setValues]= useState(key,initialValues);
+    const [showSuccessMessage, setShowSuccessMessage] = useState(false);
+
+    const handleChanges = (e) => {
+        setValues({ ...values, [e.target.name]: e.target.value });
+      };
+    
+      const handleSubmit = (e) => {
+        e.preventDefault();
+        setShowSuccessMessage(true);
+        console.log('form submitted and data is ',values)
+      };
+
+
+    return [values,handleChanges,showSuccessMessage,handleSubmit]
+
+}
