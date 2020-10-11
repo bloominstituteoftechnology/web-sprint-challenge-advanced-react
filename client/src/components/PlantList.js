45179aff05538baf3ab5ade09cf1c7cc4481ccd4
Damaris Garcia <damaris-garcia@lambastudents.com>
2020-10-11 16:56:12 -0400

    PlantList.js Complete

--- client/src/components/PlantList.js
+++ client/src/components/PlantList.js
@@ -3,10 +3,26 @@ import axios from "axios";
 
 export default class PlantList extends Component {
   // add state with a property called "plants" - initialize as an empty array
-
+  constructor() {
+    
+    super();
+    this.state = {
+      plants: []
+    }
+  }
   // when the component mounts:
   //   - fetch data from the server endpoint - http://localhost:3333/plants
   //   - set the returned plants array to this.state.plants
+  componentDidMount() {
+    console.log('Component Mounted');
+    axios
+      .get("http://localhost:3333/plants")
+      .then((res) => {
+        console.log(res, "The Response");
+        this.setState({plants: res.data.plantsData})
+      })
+      .catch((err) => console.log(err, "Caught and Error"))
+  }
 
   /*********  DON'T CHANGE ANYTHING IN THE RENDER FUNCTION *********/
   render() {
