c08569726048ddb0cb57d9a8f6ec3ec7bab42c2a
Damaris Garcia <damaris-garcia@lambastudents.com>
2020-10-11 18:47:40 -0400

    Testing Complete

--- client/src/components/CheckoutForm.test.js
+++ client/src/components/CheckoutForm.test.js
@@ -1,9 +1,18 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { fireEvent, getAllByTestId, getByText, render, screen } from "@testing-library/react";
 import CheckoutForm from "./CheckoutForm";
 
 // Write up the two tests here and make sure they are testing what the title shows
 
-test("form header renders", () => {});
+test("form header renders", () => {
+    render(<CheckoutForm />)
+    const header = screen.getByText(/Checkout Form/)
+    expect(header).toBeInTheDocument();
+});
 
-test("form shows success message on submit with form details", () => {});
+test("form shows success message on submit with form details", () => {
+    render(<CheckoutForm />)
+    const element = screen.getAllByTestId('successMessage')
+    fireEvent.click(getByText('Checkout', element))
+
+});
