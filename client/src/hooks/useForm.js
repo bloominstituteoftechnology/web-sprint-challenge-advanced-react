3b2395c6c1ece16cab892023591326e39d48e476
Damaris Garcia <damaris-garcia@lambastudents.com>
2020-10-11 17:59:29 -0400

    Checkout form has loaded working on correcting inputs

--- client/src/hooks/useForm.js
+++ client/src/hooks/useForm.js
@@ -1,1 +1,23 @@
 // write your custom hook here to control your checkout form
+import { useState } from "react";
+
+
+
+export const useForm = (key, initialValue) => {
+    const [values, setValues] = useState(key, initialValue)
+    const [showSuccessMessage, setShowSuccessMessage] = useState(false);
+   
+
+    
+    const handleChanges = (e) => {
+    setValues({ ...values, [e.target.name]: e.target.value });
+    };
+
+    const handleSubmit = (e) => {
+    e.preventDefault();
+    setShowSuccessMessage(true);
+    };
+
+    return [values, handleChanges, handleSubmit, showSuccessMessage];
+
+}
