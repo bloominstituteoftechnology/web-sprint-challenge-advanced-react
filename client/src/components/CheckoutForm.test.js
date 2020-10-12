934b6d81c0b9fa596abbf7a9240d87f0494049b2
Terry Edwards <terryedwardsjr113@gmail.com>
2020-10-11 20:16:04 -0400

    mvp

--- client/src/components/CheckoutForm.test.js
+++ client/src/components/CheckoutForm.test.js
@@ -20,38 +20,37 @@ const city=screen.getByLabelText(/city/i);
 const state=screen.getByLabelText(/state/i);
 const zip=screen.getByLabelText(/zip/i);
 const button=screen.getByRole('button',{name:/checkout/i});
-const message=()=>{return(
-    <div className="success-message" data-testid="successMessage">
-          <p>
-            You have ordered some plants! Woo-hoo! <span role="img">🎉</span>
-          </p>
-          <p>Your new green friends will be shipped to:</p>
-          <br />
-          <br />
-          <p>
-            Terry Edwards
-          </p>
-          <p>3555 sedona</p>
-          <p>
-            Tally, FL 323
-          </p>
-        </div>
-)}
-
 
 // act
-fireEvent.change(firstName,{tarrget:{value:'Terry'}});
-fireEvent.change(lastName,{tarrget:{value:'Edwards'}});
-fireEvent.change(address,{tarrget:{value:'3555 sedona'}});
-fireEvent.change(city,{tarrget:{value:'Tally'}});
-fireEvent.change(state,{tarrget:{value:'FL'}});
-fireEvent.change(zip,{tarrget:{value:'323'}});
+fireEvent.change(firstName,{target:{value:'Terry'}});
+fireEvent.change(lastName,{target:{value:'Edwards'}});
+fireEvent.change(address,{target:{value:'3555 sedona'}});
+fireEvent.change(city,{target:{value:'Tally'}});
+fireEvent.change(state,{target:{value:'FL'}});
+fireEvent.change(zip,{target:{value:'323'}});
 fireEvent.click(button)
 
 // assert 
-const success= await screen.findByTestId(/successMessage/i);
-expect(success).toHaveTextContent('You have ordered some plants! Woo-hoo! 🎉Your new green friends will be shipped to: Terry Edwards 3555 sedona Tally, FL 323')
+const firstNameInput= await screen.findByText(/terry/i)
+const lastNameInput= await screen.findByText(/edwards/i)
+const addressInput= await screen.findByText(/3555 sedona/i)
+const cityInput= await screen.findByText(/tally/i)
+const stateInput= await screen.findByText(/Fl/i)
+const zipInput= await screen.findByText(/323/i)
+const successLine1= await screen.findByText(/You have ordered some plants!/i);
+const successLine2= await screen.findByText(/Your new green friends will be shipped to:/i);
+
+expect(successLine1).toHaveTextContent(/You have ordered some plants!/i)
+expect(successLine2).toHaveTextContent(/Your new green friends will be shipped to:/i)
+expect(firstNameInput).toHaveTextContent(/terry/i)
+expect(lastNameInput).toHaveTextContent(/edwards/i)
+expect(addressInput).toHaveTextContent(/3555 sedona/i)
+expect(cityInput).toHaveTextContent(/tally/i)
+expect(stateInput).toHaveTextContent(/Fl/i)
+expect(zipInput).toHaveTextContent(/323/i)
+
+
+
 
 });
 
-// Terry Edwards 3555 sedona Tally, FL 323
