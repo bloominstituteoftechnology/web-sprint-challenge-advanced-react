f58aa4502697690e92e2d894cb186a688c57c176
Brian Abeyta-Pratt <babeytapratt@gmail.com>
2020-10-30 11:54:21 -0600

    finished useForm

--- client/src/components/CheckoutForm.js
+++ client/src/components/CheckoutForm.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import useForm from '../hooks/useForm'
 
 const initialValue = {
   firstName: "",
@@ -9,17 +10,11 @@ const initialValue = {
   zip: "",
 };
 
-// This form should be handled by a "useForm" custom hook
-// Build out the logic needed for a form custom hook (see the useForm.js file)
-// and replace the necessary stateful logic from CheckoutForm with the hook
+
 
 const CheckoutForm = (props) => {
   const [showSuccessMessage, setShowSuccessMessage] = useState(false);
-  const [values, setValues] = useState(initialValue);
-
-  const handleChanges = (e) => {
-    setValues({ ...values, [e.target.name]: e.target.value });
-  };
+  const [formValue, handleChanges, clearForm] = useForm(initialValue);
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -34,7 +29,7 @@ const CheckoutForm = (props) => {
           First Name:
           <input
             name="firstName"
-            value={values.firstName}
+            value={formValue.firstName}
             onChange={handleChanges}
           />
         </label>
@@ -42,7 +37,7 @@ const CheckoutForm = (props) => {
           Last Name:
           <input
             name="lastName"
-            value={values.lastName}
+            value={formValue.lastName}
             onChange={handleChanges}
           />
         </label>
@@ -50,21 +45,21 @@ const CheckoutForm = (props) => {
           Address:
           <input
             name="address"
-            value={values.address}
+            value={formValue.address}
             onChange={handleChanges}
           />
         </label>
         <label>
           City:
-          <input name="city" value={values.city} onChange={handleChanges} />
+          <input name="city" value={formValue.city} onChange={handleChanges} />
         </label>
         <label>
           State:
-          <input name="state" value={values.state} onChange={handleChanges} />
+          <input name="state" value={formValue.state} onChange={handleChanges} />
         </label>
         <label>
           Zip:
-          <input name="zip" value={values.zip} onChange={handleChanges} />
+          <input name="zip" value={formValue.zip} onChange={handleChanges} />
         </label>
         <button>Checkout</button>
       </form>
@@ -78,11 +73,11 @@ const CheckoutForm = (props) => {
           <br />
           <br />
           <p>
-            {values.firstName} {values.lastName}
+            {formValue.firstName} {formValue.lastName}
           </p>
-          <p>{values.address}</p>
+          <p>{formValue.address}</p>
           <p>
-            {values.city}, {values.state} {values.zip}
+            {formValue.city}, {formValue.state} {formValue.zip}
           </p>
         </div>
       )}
--- client/src/hooks/useForm.js
+++ client/src/hooks/useForm.js
@@ -1,1 +1,22 @@
 // write your custom hook here to control your checkout form
+import React, { useState } from 'react';
+
+const useForm = (initialValue) => {
+    const [formValue, setFormValue] = useState("form", initialValue);
+
+    const handleChanges = e => {
+        setFormValue({
+            ...formValue,
+            [e.target.name]:e.target.value
+        })
+    }
+
+    const clearForm = e => {
+        e.preventDefault();
+        setFormValue(initialValue);
+    };
+
+    return([formValue, handleChanges, clearForm]);
+}
+
+export default useForm;
