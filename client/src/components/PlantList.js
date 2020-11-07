1d26d1df7b11421c7be472d425c45bcd77dfe52d
Antoinette Jones <aajones71@gmail.com>
2020-11-06 23:42:40 -0500

    added state?

--- client/src/components/PlantList.js
+++ client/src/components/PlantList.js
@@ -3,7 +3,12 @@ import axios from "axios";
 
 export default class PlantList extends Component {
   // add state with a property called "plants" - initialize as an empty array
-
+  constructor(){
+    super();
+    this.state={
+     plants: plants = []
+    }
+  }
   // when the component mounts:
   //   - fetch data from the server endpoint - http://localhost:3333/plants
   //   - set the returned plants array to this.state.plants
