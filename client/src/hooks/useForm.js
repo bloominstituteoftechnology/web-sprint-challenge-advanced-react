1c71a6dd9dedafb05897bfa0f1059305d7b4b70d
Austin Littlefield <austinlittlefield@MacBook-Pro.local>
2020-11-08 13:36:50 -0800

    UseFrom imported and working

--- client/src/hooks/useForm.js
+++ client/src/hooks/useForm.js
@@ -1,1 +1,9 @@
 // write your custom hook here to control your checkout form
+import {useState} from "react"
+
+export const useForm = (initialValue) => {
+    const [values, setValues] = useState(initialValue);
+
+
+    return [values, setValues]
+}
