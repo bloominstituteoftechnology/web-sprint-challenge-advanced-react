f2f2e72f902c80b8b20be2c706e4c4f57bed1a34
Aaron <54789688+flowersa29@users.noreply.github.com>
2020-11-17 19:08:12 -0600

    testing

--- client/src/components/CheckoutForm.test.js
+++ client/src/components/CheckoutForm.test.js
@@ -1,9 +1,32 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, fireEvent } from "@testing-library/react";
 import CheckoutForm from "./CheckoutForm";
 
 // Write up the two tests here and make sure they are testing what the title shows
 
-test("form header renders", () => {});
+test("form header renders", () => {
+  //Arrange
+  const {getByText} = render(<CheckoutForm/>)
+  const header = getByText(/Checkout Form/i)
+  //assert
+  expect(header).toBeInTheDocument()
+});
 
-test("form shows success message on submit with form details", () => {});
+test("form shows success message on submit with form details", async () => {
+  //Arrange
+  const {getByText} = render(<CheckoutForm/>)
+  //Act
+  const order = getByText(/Zip/i)
+
+  const button = getByText(/CHECKOUT/i)
+
+  fireEvent.click(button)
+
+  //Assert
+  expect(order).toHaveTextContent("Aaron")
+
+
+
+
+
+});
