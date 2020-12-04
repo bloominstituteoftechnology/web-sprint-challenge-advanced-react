d0272be71be1b09f6b67452de48dceb04ef389ac
Ian Luchini <iancarloluchini@gmail.com>
2020-12-04 12:16:38 -0500

    fetched the data from the local server, and created a custom hook to deal with the stateful logic in the checkout form (not in local storage)

--- client/src/hooks/useForm.js
+++ client/src/hooks/useForm.js
@@ -1,1 +1,20 @@
 // write your custom hook here to control your checkout form
+import React, { useState } from 'react';
+
+const useForm = (initVal) => {
+    const [showSuccessMessage, setShowSuccessMessage] = useState(false);
+    const [values, setValues] = useState(initVal);
+
+    const handleChanges = (e) => {
+        setValues({ ...values, [e.target.name]: e.target.value });
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setShowSuccessMessage(true);
+    };
+
+    return [values, handleChanges, handleSubmit, showSuccessMessage];
+}
+
+export default useForm;
