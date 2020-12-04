f334296cb2513355e405a1d890d953916dec92d6
DavidHall-Code <david_m_hall@outlook.com>
2020-12-04 13:19:54 -0500

    wrtoe to test. Both passed

--- client/src/components/CheckoutForm.test.js
+++ client/src/components/CheckoutForm.test.js
@@ -1,9 +1,44 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import CheckoutForm from "./CheckoutForm";
 
 // Write up the two tests here and make sure they are testing what the title shows
 
-test("form header renders", () => {});
+test("form header renders", () => {
+    const {getByText} = render(<CheckoutForm />)
 
-test("form shows success message on submit with form details", () => {});
+    const header = getByText(/Checkout Form/i)
+    expect (header).toBeInTheDocument()
+});
+
+
+test("form shows success message on submit with form details", async () => {
+    render(<CheckoutForm />)
+
+const firstName = screen.getByLabelText(/first name/i);
+const lastName = screen.getByLabelText(/last name/i);
+const address = screen.getByLabelText(/address/i);
+const city = screen.getByLabelText(/city/i);
+const state = screen.getByLabelText(/state/i);
+const zipCode = screen.getByLabelText(/zip/i);
+
+fireEvent.change(firstName, {target: { value: "David"}});
+fireEvent.change(lastName, {target: { value: "Hall"}});
+fireEvent.change(address, {target: { value: "123 Empire Way"}});
+fireEvent.change(city, {target: { value: "Waterville"}});
+fireEvent.change(state, {target: { value: "Maine"}});
+fireEvent.change(zipCode, {target: { value: "04901"}});
+
+expect(firstName).toHaveValue("David"); 
+
+const button = screen.getByRole("button", {name: /checkout/i});
+fireEvent.click(button);
+//after filling out the fom the state changed to true in my terminal and then when grabbing and checking for the seccess message the test passed
+
+const successMessage = await screen.getByText(/you have ordered/i);
+expect(successMessage).toBeInTheDocument();
+})
+
+
+
+
