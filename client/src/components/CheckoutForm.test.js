a8412fead690df568ea6a5448ba207b8d6470327
River Smith <rivyyriv@gmail.com>
2020-12-12 11:13:32 -0700

    Tests

--- client/src/components/CheckoutForm.test.js
+++ client/src/components/CheckoutForm.test.js
@@ -4,6 +4,47 @@ import CheckoutForm from "./CheckoutForm";
 
 // Write up the two tests here and make sure they are testing what the title shows
 
-test("form header renders", () => {});
+test("form header renders", () => {
 
-test("form shows success message on submit with form details", () => {});
+    const { getByText } = render(<CheckoutForm />);
+
+    const linkElement = getByText(/Checkout Form/i);
+
+    expect(linkElement).toBeInTheDocument();
+
+});
+
+   
+
+test("form shows success message on submit with form details", () => {
+
+    const firstNameInput = screen.getByPlaceholderText(/first name here/i);
+    const lastNameInput = screen.getByPlaceholderText(/last name here/i);
+    const addressInput = screen.getByPlaceholderText(/address here/i);
+    const cityInput = screen.getByPlaceholderText(/city here/i);
+    const stateInput = screen.getByPlaceholderText(/state here/i);
+    const zipInput = screen.getByPlaceholderText(/zip code here/i);
+  
+  
+    fireEvent.change(firstNameInput, {target:{name:'firstName',value:"River"}});
+    fireEvent.change(lastNameInput, {target:{name:'lastName',value:"Smith"}});
+    fireEvent.change(addressInput, {target:{name:'address',value:"Example Drive"}});
+    fireEvent.change(cityInput, {target:{name:'city',value:"Eagle Mountain"}});
+    fireEvent.change(stateInput,{target: {name:'state', value: 'UT'}});
+    fireEvent.change(zipInput, {target: {name:'zip', value:'84005'}});
+  
+  
+  
+    const submitButton = screen.getByTestId(/button/i);
+    fireEvent.click(submitButton);
+  
+    const successMessage= screen.getByTestId(/successMessage/i);
+    expect(successMessage).toBeInTheDocument();
+    expect(firstNameInput).toBeInTheDocument(successMessage);
+    expect(lastNameInput).toBeInTheDocument(successMessage);
+    expect(addressInput).toBeInTheDocument(successMessage);
+    expect(cityInput).toBeInTheDocument(successMessage);
+    expect(stateInput).toBeInTheDocument(successMessage);
+    expect(zipInput).toBeInTheDocument(successMessage);
+
+});
