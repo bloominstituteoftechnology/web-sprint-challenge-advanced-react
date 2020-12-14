76a251df1f46726219468338a24d7de094a40dc3
Ahmad Wali Sayadi <ahmadwalisayadi@Ahmads-MacBook-Pro.local>
2020-12-13 17:40:52 -0800

    project completed

--- client/src/components/CheckoutForm.js
+++ client/src/components/CheckoutForm.js
@@ -1,30 +1,38 @@
-import React, { useState } from "react";
+import React, { useState } from 'react'
+import { useForm } from '../hooks/useForm'
 
 const initialValue = {
-  firstName: "",
-  lastName: "",
-  address: "",
-  city: "",
-  state: "",
-  zip: "",
-};
+  firstName: '',
+  lastName: '',
+  address: '',
+  city: '',
+  state: '',
+  zip: ''
+}
 
 // This form should be handled by a "useForm" custom hook
 // Build out the logic needed for a form custom hook (see the useForm.js file)
 // and replace the necessary stateful logic from CheckoutForm with the hook
 
-const CheckoutForm = (props) => {
-  const [showSuccessMessage, setShowSuccessMessage] = useState(false);
-  const [values, setValues] = useState(initialValue);
+const CheckoutForm = props => {
+  // const [showSuccessMessage, setShowSuccessMessage] = useState(false);
+  // const [values, setValues] = useState(initialValue);
 
-  const handleChanges = (e) => {
-    setValues({ ...values, [e.target.name]: e.target.value });
-  };
+  // const handleChanges = (e) => {
+  //   setValues({ ...values, [e.target.name]: e.target.value });
+  // };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    setShowSuccessMessage(true);
-  };
+  const [
+    showSuccessMessage,
+    setShowSuccessMessage,
+    values,
+    handleChanges
+  ] = useForm(initialValue)
+
+  const handleSubmit = e => {
+    e.preventDefault()
+    setShowSuccessMessage(true)
+  }
 
   return (
     <>
@@ -33,7 +41,7 @@ const CheckoutForm = (props) => {
         <label>
           First Name:
           <input
-            name="firstName"
+            name='firstName'
             value={values.firstName}
             onChange={handleChanges}
           />
@@ -41,7 +49,7 @@ const CheckoutForm = (props) => {
         <label>
           Last Name:
           <input
-            name="lastName"
+            name='lastName'
             value={values.lastName}
             onChange={handleChanges}
           />
@@ -49,30 +57,30 @@ const CheckoutForm = (props) => {
         <label>
           Address:
           <input
-            name="address"
+            name='address'
             value={values.address}
             onChange={handleChanges}
           />
         </label>
         <label>
           City:
-          <input name="city" value={values.city} onChange={handleChanges} />
+          <input name='city' value={values.city} onChange={handleChanges} />
         </label>
         <label>
           State:
-          <input name="state" value={values.state} onChange={handleChanges} />
+          <input name='state' value={values.state} onChange={handleChanges} />
         </label>
         <label>
           Zip:
-          <input name="zip" value={values.zip} onChange={handleChanges} />
+          <input name='zip' value={values.zip} onChange={handleChanges} />
         </label>
         <button>Checkout</button>
       </form>
 
       {showSuccessMessage && (
-        <div className="success-message" data-testid="successMessage">
+        <div className='success-message' data-testid='successMessage'>
           <p>
-            You have ordered some plants! Woo-hoo! <span role="img">🎉</span>
+            You have ordered some plants! Woo-hoo! <span role='img'>🎉</span>
           </p>
           <p>Your new green friends will be shipped to:</p>
           <br />
@@ -87,7 +95,7 @@ const CheckoutForm = (props) => {
         </div>
       )}
     </>
-  );
-};
+  )
+}
 
-export default CheckoutForm;
+export default CheckoutForm
--- client/src/hooks/useForm.js
+++ client/src/hooks/useForm.js
@@ -1,1 +1,14 @@
-// write your custom hook here to control your checkout form
+
+import { useState } from "react";
+
+export const useForm = (initialValue) => {
+  const [showSuccessMessage, setShowSuccessMessage] = useState(false);
+
+  const [values, setValues] = useState(initialValue);
+
+  const handleChanges = (e) => {
+    setValues({ ...values, [e.target.name]: e.target.value });
+  };
+
+  return [ showSuccessMessage, setShowSuccessMessage,values, handleChanges];
+};
