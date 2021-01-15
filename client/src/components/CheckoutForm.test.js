d703cfe8da567b526cf0102a84d697f91379add7
brandon santos <brandonx1123@gmail.com>
2021-01-15 14:29:09 -0500

    finshed the second test.. So MVP done(all because of a lowecaseb)

--- client/src/components/CheckoutForm.test.js
+++ client/src/components/CheckoutForm.test.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
 import CheckoutForm from "./CheckoutForm";
+import userEvent from '@testing-library/user-event'
 
 // Write up the two tests here and make sure they are testing what the title shows
 
@@ -8,19 +9,37 @@ test("form header renders", () => {
 
     render (<CheckoutForm />)
 
-    const HeaderInput = screen.getByText(/Checkout Form/i)
-
+    const headerInput = screen.getByText(/Checkout Form/i)
+    expect(headerInput).toBeInTheDocument()
 });
 
 
 
 
-test("form shows success message on submit with form details", () => {
+test("form shows success message on submit with form details",  async () => {
 
     render(<CheckoutForm />)
 
-    const successMessage = screen.getByTestId('successMessage')
+    const firstNameInput = screen.getByLabelText(/first name/i) 
+    const lastNameInput = screen.getByLabelText(/last name/i) 
+    const addressInput = screen.getByLabelText(/address/i) 
+    const cityInput = screen.getByLabelText(/city/i) 
+    const stateInput = screen.getByLabelText(/state/i) 
+    const zipInput = screen.getByLabelText(/zip/i) 
+    
 
+    userEvent.type(firstNameInput, 'Brandon')
+    userEvent.type(lastNameInput, 'Santos')
+    userEvent.type(addressInput, '365 Blue St')
+    userEvent.type(cityInput, 'Westbury')
+    userEvent.type(stateInput, 'NYC')
+    userEvent.type(zipInput, '11503')
+
+    const submitButton = screen.getByRole('button')
+    userEvent.click(submitButton)
+
+    const customerCheckout = await screen.findByText(/You have ordered some plants! woo-hoo!/i);
+    expect(customerCheckout).toBeInTheDocument()
 });
 
 
