48926e21a6c6d49f95bb0c4676673a9fb8351963
Diego Roman <diegormnv209@gmail.com>
2021-01-15 08:26:24 -0800

    plant list is displaying

--- client/src/components/PlantList.js
+++ client/src/components/PlantList.js
@@ -3,10 +3,26 @@ import axios from "axios";
 
 export default class PlantList extends Component {
   // add state with a property called "plants" - initialize as an empty array
+  state={
+      plants: []
+    }
+  
 
   // when the component mounts:
-  //   - fetch data from the server endpoint - http://localhost:3333/plants
+  //   - fetch data from the server endpoint - 
   //   - set the returned plants array to this.state.plants
+  componentDidMount(){
+    axios.get('http://localhost:3333/plants')
+    .then((res) =>{
+      console.log(res,'hi');
+      this.setState({
+        plants: res.data.plantsData
+      })
+    })
+    .catch((err) =>{
+            console.log(err,'error')
+          })
+  }
 
   /*********  DON'T CHANGE ANYTHING IN THE RENDER FUNCTION *********/
   render() {
