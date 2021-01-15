ebaae890189d21e5338eaa3a8f5fbd6f123da5c7
Brennen O'Connor <brennenoconnor@Brennens-MacBook-Pro.local>
2021-01-15 13:46:49 -0500

    All set

--- client/src/components/CheckoutForm.test.js
+++ client/src/components/CheckoutForm.test.js
@@ -1,9 +1,40 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, screen, fireEvent} from "@testing-library/react";
 import CheckoutForm from "./CheckoutForm";
 
 // Write up the two tests here and make sure they are testing what the title shows
 
-test("form header renders", () => {});
+test("form header renders", () => {
+render(<CheckoutForm />) // Render the form
+expect(screen.getByText(/checkout form/i)); // Find the header by the regEx text
 
-test("form shows success message on submit with form details", () => {});
+});
+
+test("form shows success message on submit with form details", () => {
+    render(<CheckoutForm />)
+    // query all inputs
+    const firstNameInput = screen.getByLabelText(/first name/i);
+    const lastNameInput = screen.getByLabelText(/last name/i);
+    const addressInput = screen.getByLabelText(/address/i);
+    const cityInput = screen.getByLabelText(/city/i);
+    const stateInput = screen.getByLabelText(/state/i);
+    const zipInput = screen.getByLabelText(/zip/i);
+    const submitBtn = screen.getByRole('button', {name: /checkout/i});
+
+// Fire events
+    fireEvent.change(firstNameInput, {target: { value: 'Jen' }});
+    fireEvent.change(lastNameInput, {target: { value: 'Stewart' }});
+    fireEvent.change(addressInput, {target: { value: '5555 Road rd.' }});
+    fireEvent.change(cityInput, {target: { value: 'Kansas City' }});
+    fireEvent.change(stateInput, {target: { value: 'KS' }});
+    fireEvent.change(zipInput, {target: { value: '66219' }});
+// Fire Submit Button
+    fireEvent.click(submitBtn);
+    expect(screen.getByText(/jen/i))
+    expect(screen.getByText(/stewart/i))
+    expect(screen.getByText(/555 Road rd./i))
+    expect(screen.getByText(/Kansas City/i))
+    expect(screen.getByText(/66219/i))
+    expect(screen.queryByText('successMessage'))
+
+});
--- client/src/components/PlantList.js
+++ client/src/components/PlantList.js
@@ -3,7 +3,21 @@ import axios from "axios";
 
 export default class PlantList extends Component {
   // add state with a property called "plants" - initialize as an empty array
-
+  constructor() {
+    super();
+    this.state = {
+      plants: [],
+    };
+  }
+  // when the component mounts:
+  //   - fetch data from the server endpoint - http://localhost:3333/plants
+  //   - set the returned plants array to this.state.plants
+  componentDidMount() {
+    axios.get('http://localhost:3333/plants').then((res) => {
+      console.log(res);
+      this.setState({ plants: res.data.plantsData });
+    });
+  }
   // when the component mounts:
   //   - fetch data from the server endpoint - http://localhost:3333/plants
   //   - set the returned plants array to this.state.plants
@@ -13,7 +27,7 @@ export default class PlantList extends Component {
     return (
       <main className="plant-list">
         {this.state?.plants?.map((plant) => (
-          <div className="plant-card" key={plant.id} data-testid="plant-card">
+          <div className="plant-card" key={plant.id}>
             <img className="plant-image" src={plant.img} alt={plant.name} />
             <div className="plant-details">
               <h2 className="plant-name">{plant.name}</h2>
