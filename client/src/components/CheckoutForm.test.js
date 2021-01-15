cd014b0520b497899776d9c6f8231c1714be0ee9
Jacobstephens17 <64627086+Jacobstephens17@users.noreply.github.com>
2021-01-15 09:22:12 -0800

    got tests working

--- client/src/components/CheckoutForm.test.js
+++ client/src/components/CheckoutForm.test.js
@@ -1,9 +1,38 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
+import userEvent from '@testing-library/user-event'
 import CheckoutForm from "./CheckoutForm";
+
 
 // Write up the two tests here and make sure they are testing what the title shows
 
-test("form header renders", () => {});
+test("form header renders", () => {
+    render(<CheckoutForm/>)
+    const header = screen.queryByText(/checkout form/i)
+    expect(header).toBeInTheDocument();
+});
 
-test("form shows success message on submit with form details", () => {});
+test("form shows success message on submit with form details", () => {
+    render(<CheckoutForm />);
+  
+    const firstNameInput = screen.getByLabelText(/first name/i);
+    const lastNameInput = screen.getByLabelText(/last name/i);
+    const addressInput = screen.getByLabelText(/address/i);
+    const cityInput = screen.getByLabelText(/city/i);
+    const stateInput = screen.getByLabelText(/state/i);
+    const zipInput = screen.getByLabelText(/zip/i);
+    const button = screen.getByRole("button");
+  
+    userEvent.type(firstNameInput, "Jacob");
+    userEvent.type(lastNameInput, "Stephens");
+    userEvent.type(addressInput, "lkajdfl;");
+    userEvent.type(cityInput, "Las Vegas");
+    userEvent.type(stateInput, "NV");
+    userEvent.type(zipInput,"89144");
+  
+    userEvent.click(button);
+  
+    const successMessage = screen.getByText(/woo-hoo/i);
+    expect(successMessage).toBeVisible();
+  });
+  
