d5d2737a2d36bb7ae075ab9e19c9ad8d815cc363
Gatien Orti-vinas <gatien.orti@gmail.com>
2021-01-15 12:07:17 -0500

    finish

--- client/src/components/CheckoutForm.test.js
+++ client/src/components/CheckoutForm.test.js
@@ -1,9 +1,58 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render,screen,fireEvent } from "@testing-library/react";
 import CheckoutForm from "./CheckoutForm";
+import userEvent from '@testing-library/user-event'
 
 // Write up the two tests here and make sure they are testing what the title shows
 
-test("form header renders", () => {});
+test("form header renders", () => {
+    render(<CheckoutForm/>)
+    
+    const fnameInput = screen.getByLabelText(/first name/i)
+    const lnameInput = screen.getByLabelText(/last name/i)
+    const addressInput = screen.getByLabelText(/address/i)
+    const cityInput = screen.getByLabelText(/city/i)
+    const stateInput = screen.getByLabelText(/state/i)
+    const zipInput = screen.getByLabelText(/zip/i)
 
-test("form shows success message on submit with form details", () => {});
+    userEvent.type(fnameInput,'someone')
+    userEvent.type(lnameInput,'someonelastName')
+    userEvent.type(addressInput,'somewhere')
+    userEvent.type(cityInput,'somewhereCity')
+    userEvent.type(stateInput,'somewhereState')
+    userEvent.type(zipInput,'somwhereZip')
+
+    expect(fnameInput.value).toEqual("someone")
+    expect(lnameInput.value).toEqual('someonelastName')
+    expect(addressInput.value).toEqual('somewhere')
+    expect(cityInput.value).toEqual('somewhereCity')
+    expect(stateInput.value).toEqual('somewhereState')
+    expect(zipInput.value).toEqual('somwhereZip')
+});
+
+test("form shows success message on submit with form details", () => {
+    render(<CheckoutForm/>)
+
+    const fnameInput = screen.getByLabelText(/first name/i)
+    const lnameInput = screen.getByLabelText(/last name/i)
+    const addressInput = screen.getByLabelText(/address/i)
+    const cityInput = screen.getByLabelText(/city/i)
+    const stateInput = screen.getByLabelText(/state/i)
+    const zipInput = screen.getByLabelText(/zip/i)
+
+    userEvent.type(fnameInput,'someone')
+    userEvent.type(lnameInput,'someonelastName')
+    userEvent.type(addressInput,'somewhere')
+    userEvent.type(cityInput,'somewhereCity')
+    userEvent.type(stateInput,'somewhereState')
+    userEvent.type(zipInput,'somwhereZip')
+
+    const checkoutButton = screen.getByRole('button')
+    userEvent.click(checkoutButton)
+    
+    const checkoutFname = screen.queryByText(/someone/i)
+    const successMessage= screen.queryByTestId(/successMessage/i);
+
+    expect(checkoutFname).toBeInTheDocument(successMessage)
+   expect(successMessage).toBeInTheDocument()
+});
--- client/src/hooks/useForm.js
+++ client/src/hooks/useForm.js
@@ -1,1 +1,15 @@
 // write your custom hook here to control your checkout form
+import React, {useState} from 'react'
+
+
+function useForm (initialValue){
+    const [showSuccessMessage, setShowSuccessMessage] = useState(false);
+    const [values, setValues] = useState(initialValue);
+    
+    const handleChanges = (e) => {
+        setValues({ ...values, [e.target.name]: e.target.value });
+      };
+
+return[showSuccessMessage,setShowSuccessMessage,values, handleChanges]
+}
+export default useForm
