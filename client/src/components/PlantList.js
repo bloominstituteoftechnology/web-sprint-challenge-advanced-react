837d9ee2ab10d77e872cf4234a2303d440d77f23
Andrew Sohrabi <andrewsohrabi@Andrews-MacBook-Pro.local>
2021-01-15 16:03:59 -0800

    checkout form works w/ custom hook

--- client/src/components/PlantList.js
+++ client/src/components/PlantList.js
@@ -1,12 +1,31 @@
-import React, { Component } from "react";
+import React, { Component, useState } from "react";
 import axios from "axios";
 
 export default class PlantList extends Component {
   // add state with a property called "plants" - initialize as an empty array
 
+  state = {
+    plants: [],
+  };
+
   // when the component mounts:
   //   - fetch data from the server endpoint - http://localhost:3333/plants
   //   - set the returned plants array to this.state.plants
+
+  componentDidMount() {
+    axios.get('http://localhost:3333/plants')
+      .then((res)=> {
+        this.setState({
+          plants: res.data.plantsData
+        })
+      })
+      .catch((err) => {
+        console.log("this is your error:",err)
+      })
+  }
+
+  
+
 
   /*********  DON'T CHANGE ANYTHING IN THE RENDER FUNCTION *********/
   render() {
--- client/src/hooks/useForm.js
+++ client/src/hooks/useForm.js
@@ -1,1 +1,51 @@
 // write your custom hook here to control your checkout form
+
+// import { useState } from 'react';
+
+// const useForm = () => {
+
+//     const [showSuccessMessage, setShowSuccessMessage] = useState(false);
+
+//     const [values, setValues] = useState(initialValue);
+
+//     const handleChanges = (e) => {
+//         setValues({ ...values, [e.target.name]: e.target.value });
+//     };
+
+//     const handleSubmit = (e) => {
+//         e.preventDefault();
+//         setShowSuccessMessage(true);
+//     };
+
+//     const clearForm = e => {
+//         e.preventDefault();
+//         setValues(initialValues);
+//       };
+
+//     return ([values,showSuccessMessage,handleChanges,handleSubmit,clearForm]);
+// }
+
+// export default useForm;
+
+import { useState } from 'react';
+
+export default function useForm(initialValue) {
+  const [showSuccessMessage, setShowSuccessMessage] = useState(false);
+  const [values, setValues] = useState(initialValue);
+
+  const handleChanges = (e) => {
+    setValues({ ...values, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setShowSuccessMessage(true);
+  };
+
+    const clearForm = e => {
+        e.preventDefault();
+        setValues(initialValue);
+    };
+
+  return ([values, showSuccessMessage, handleChanges, handleSubmit, clearForm]);
+}
