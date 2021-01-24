830b8d7afc9abec37749cfdb4c4594dafa80a5e2
bipin-shrestha <iambpn.shrestha@gmail.com>
2021-01-24 11:19:08 -0800

    PlantList components is fetching data from server

--- client/src/components/PlantList.js
+++ client/src/components/PlantList.js
@@ -3,7 +3,23 @@ import axios from "axios";
 
 export default class PlantList extends Component {
   // add state with a property called "plants" - initialize as an empty array
+ constructor(){
+   super();
+   this.state ={
+     plants: []
+   }
+  }
+  
+  componentDidMount(){
+    axios.get(`http://localhost:3333/plants`)
+          .then(res => {
+            console.log(`lets see the data here!!!!!!!!!!`, res.data);
+            this.setState({ plants : res.data.plantsData})
+          })
+          .catch(err => console.error(err));
+  }
 
+ 
   // when the component mounts:
   //   - fetch data from the server endpoint - http://localhost:3333/plants
   //   - set the returned plants array to this.state.plants
