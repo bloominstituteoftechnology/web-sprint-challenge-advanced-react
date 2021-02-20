6523db06e56f7d1d2823a52b4e55d06de53e5cc8
Tony Miller <70006926+artofmayhem@users.noreply.github.com>
2021-02-20 13:10:42 -1000

    initial blind commit awaiting canvas issue answer

--- src/components/PlantList.js
+++ src/components/PlantList.js
@@ -1,12 +1,28 @@
-import React, { Component } from "react";
+import React, { Component, useState } from "react";
 import axios from "axios";
 
 export default class PlantList extends Component {
   // add state with a property called "plants" - initialize as an empty array
-
+  constructor() {
+    super();
+    this.state = {
+      plants: [],
+    };
+  }
   // when the component mounts:
   //   - fetch data from the server endpoint - http://localhost:3333/plants
   //   - set the returned plants array to this.state.plants
+  componentDidMount() {
+    axios
+      .get(`http://localhost:3333/plants.`)
+      .then((res) => {
+        console.log("Initial Data Vlaue From Axios Request", res.data);
+        this.setState((prevState) => ({ ...prevState, plants: res.data }));
+      })
+      .catch((error) => {
+        console.log("API CAlL ERROR FROM COMPONENT DIO MOUNT", error);
+      });
+  }
 
   /*********  DON'T CHANGE ANYTHING IN THE RENDER FUNCTION *********/
   render() {
--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -1,1 +1,11 @@
 // write your custom hook here to control your checkout form
+import {useState} from 'react';
+
+export const useForm = (initialValues) => {
+        const [values, setValues] = useState(initialValues)
+
+        const handleChange = (e) => {
+            setValues({ ...values})
+        }
+
+}
