38e6371624b75ad3091b2bebfb8d77a5360552c7
Kelsey Nielsen <kelseywnielsen@gmail.com>
2021-02-21 15:16:41 -0700

    added component did mount

--- src/components/PlantList.js
+++ src/components/PlantList.js
@@ -2,11 +2,24 @@ import React, { Component } from "react";
 import axios from "axios";
 
 export default class PlantList extends Component {
-  // add state with a property called "plants" - initialize as an empty array
 
-  // when the component mounts:
-  //   - fetch data from the server endpoint - http://localhost:3333/plants
-  //   - set the returned plants array to this.state.plants
+  constructor(){
+    super();
+    this.state = {
+      plants: []
+    }
+  }
+
+  componentDidMount() {
+    axios.get('http://localhost:3333/plants')
+      .then(res => {
+        console.log(res)
+        this.setState({
+          ...this.state,
+          plants: res.data,
+        })
+      })
+  }
 
   /*********  DON'T CHANGE ANYTHING IN THE RENDER FUNCTION *********/
   render() {
