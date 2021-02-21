68ab7cbd13601dab9a07e98b2fe2924fffb70347
Tony Miller <70006926+artofmayhem@users.noreply.github.com>
2021-02-20 14:43:11 -1000

    initial test done and working with sanity check

--- src/components/CheckoutForm.test.js
+++ src/components/CheckoutForm.test.js
@@ -1,11 +1,25 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import CheckoutForm from "./CheckoutForm";
 
 // Write up the two tests here and make sure they are testing what the title shows
 
 test("form header renders", () => {
     render(<CheckoutForm />);
+
+    //Arrange
+    const checkoutFormHeader = screen.getByText(/Checkout Form/i);
+
+
+
+    //Act
+    expect(checkoutFormHeader).toBeInTheDocument();
+
+
+
+    //Assert
+    expect(checkoutFormHeader).toBeVisible();
+
 });
 
 test("form shows success message on submit with form details", () => {});
