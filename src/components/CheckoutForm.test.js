78c80e925b01b9eb1f17ebf2157f57f7a8ec6912
Tony Miller <70006926+artofmayhem@users.noreply.github.com>
2021-02-20 14:59:32 -1000

    first series of tests complete and passed

--- src/components/CheckoutForm.test.js
+++ src/components/CheckoutForm.test.js
@@ -8,18 +8,40 @@ test("form header renders", () => {
     render(<CheckoutForm />);
 
     //Arrange
-    const checkoutFormHeader = screen.getByText(/Checkout Form/i);
-
+    const checkoutFormHeader = screen.getByText(/Checkout Form/i)
+    const firstName = screen.getByText(/First Name/i)
+    const lastName = screen.getByText(/Last Name/i)
+    const address = screen.getByText(/Address/i)
+    const city = screen.getByText(/City/i)
+    const state = screen.getByText(/State/i)
+    const zip = screen.getByText(/Zip/i)
+    const button = screen.getByRole('button', {name: /checkout/i}) 
 
 
     //Act
-    expect(checkoutFormHeader).toBeInTheDocument();
+    expect(checkoutFormHeader).toBeInTheDocument()
+    expect(firstName).toBeInTheDocument()
+    expect(lastName).toBeInTheDocument()
+    expect(address).toBeInTheDocument()
+    expect(city).toBeInTheDocument()
+    expect(state).toBeInTheDocument()
+    expect(zip).toBeInTheDocument()
+    expect(button).toBeInTheDocument()
 
 
 
     //Assert
-    expect(checkoutFormHeader).toBeVisible();
+    expect(checkoutFormHeader).toBeVisible()
+    expect(firstName).toBeVisible()
+    expect(lastName).toBeVisible()
+    expect(address).toBeVisible()
+    expect(city).toBeVisible()
+    expect(state).toBeVisible() 
+    expect(zip).toBeVisible()
+    expect(button).toBeVisible()
 
 });
 
-test("form shows success message on submit with form details", () => {});
+test("form shows success message on submit with form details", (
+    
+) => {});
