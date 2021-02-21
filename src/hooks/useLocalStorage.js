4fd097dd1423952393c998bb461ea4ca19ab1071
Kelsey Nielsen <kelseywnielsen@gmail.com>
2021-02-21 15:31:34 -0700

    added useLocalStorage hook

---
+++ src/hooks/useLocalStorage.js
@@ -0,0 +1,23 @@
+import { useState } from 'react';
+
+export const useLocalStorage = (key, initialValue) => {
+
+    const [value, setStateValue] = useState(() => {
+
+        if(localStorage.getItem(key)) {
+            return localStorage.getItem(key)
+        }
+        else {
+            localStorage.setItem(key, JSON.stringify(initialValue));
+            return initialValue
+        }
+    })
+
+    const setValue = newValue => {
+        setStateValue(newValue);
+        localStorage.setItem(key, JSON.stringify(newValue));
+    }
+
+    return [value, setValue]
+}
+
