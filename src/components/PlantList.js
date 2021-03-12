bb1c60007c8cef6f8422d4c4415c15f5f4457bb6
Lindell Carter <lindellcarternyc@gmail.com>
2021-03-12 11:18:06 -0500

    call api and add plants to state

--- src/components/PlantList.js
+++ src/components/PlantList.js
@@ -7,6 +7,19 @@ export default class PlantList extends Component {
   // when the component mounts:
   //   - fetch data from the server endpoint - http://localhost:3333/plants
   //   - set the returned plants array to this.state.plants
+  state = {
+    plants: []
+  }
+
+  componentDidMount() {
+    axios.get(`http://localhost:3333/plants`)
+      .then(res => {
+        this.setState({ plants: res.data })
+      })
+      .catch(err => {
+        console.log('ERROR FETCHING PLANTS', err)
+      })
+  }
 
   /*********  DON'T CHANGE ANYTHING IN THE RENDER FUNCTION *********/
   render() {
