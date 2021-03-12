8e3d9225d1ca3e4a18e78289c6fb31c757962481
Stav Corcos <poonchys@gmail.com>
2021-03-12 09:07:12 -0500

    Finished MVP for sprint

--- src/components/CheckoutForm.test.js
+++ src/components/CheckoutForm.test.js
@@ -1,9 +1,37 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { screen, render, waitFor } from "@testing-library/react";
+import userEvent from '@testing-library/user-event';
 import CheckoutForm from "./CheckoutForm";
 
 // Write up the two tests here and make sure they are testing what the title shows
 
-test("form header renders", () => {});
+test("form header renders", () => {
+    render(<CheckoutForm />)
+    const header = screen.getByText(/checkout form/i);
+    expect(header).toBeInTheDocument()
+});
 
-test("form shows success message on submit with form details", () => {});
+test("form shows success message on submit with form details", async () => {
+    render(<CheckoutForm />)
+
+    const firstName = screen.getByLabelText("First Name:");
+    userEvent.type(firstName, "stav");
+
+    const lastName = screen.getByLabelText("Last Name:");
+    userEvent.type(lastName, "corcos");
+
+    const address = screen.getByLabelText("Address:");
+    userEvent.type(address, "123 oopsie");
+
+    const city = screen.getByLabelText("City:");
+    userEvent.type(city, "lawrence");
+
+    const zip = screen.getByLabelText("Zip:");
+    userEvent.type(zip, "11559");
+
+    const button = screen.getByRole("button");
+    userEvent.click(button);
+
+    const success = screen.queryByText(/You have ordered some plants! Woo-hoo!/i);
+    expect(success).toBeInTheDocument();
+});
--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -1,1 +1,18 @@
 // write your custom hook here to control your checkout form
+import { useState } from 'react'
+function useForm(props) {
+    const [showSuccessMessage, setShowSuccessMessage] = useState(false);
+    const [values, setValues] = useState(props);
+
+    const handleChanges = (e) => {
+        setValues({ ...values, [e.target.name]: e.target.value });
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setShowSuccessMessage(true);
+    };
+  
+    return [showSuccessMessage, values, handleChanges, handleSubmit]
+}
+export default useForm
