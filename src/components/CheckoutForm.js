d2a676053e165c9ac99fc9fa82721ab8a3fa5f75
L-Steinmacher <dasai.sacul@gmail.com>
2021-03-12 09:10:44 -0800

    Hook finished starting testing

--- src/components/CheckoutForm.js
+++ src/components/CheckoutForm.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import useForm from '../hooks/useForm'
 
 const initialValue = {
   firstName: "",
@@ -15,11 +16,13 @@ const initialValue = {
 
 const CheckoutForm = (props) => {
   const [showSuccessMessage, setShowSuccessMessage] = useState(false);
-  const [values, setValues] = useState(initialValue);
+  // const [values, setValues] = useState(initialValue);
 
-  const handleChanges = (e) => {
-    setValues({ ...values, [e.target.name]: e.target.value });
-  };
+  // const handleChanges = (e) => {
+  //   setValues({ ...values, [e.target.name]: e.target.value });
+  // };
+
+  const [handleChanges, values] = useForm(initialValue)
 
   const handleSubmit = (e) => {
     e.preventDefault();
