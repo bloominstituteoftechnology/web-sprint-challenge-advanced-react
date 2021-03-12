6ff138500b073367ee426bac092803379ddaa19e
Weston Woodard <westonwoodard28@gmail.com>
2021-03-12 10:54:32 -0700

    class component and custom hook complete

--- src/components/CheckoutForm.js
+++ src/components/CheckoutForm.js
@@ -1,30 +1,14 @@
 import React, { useState } from "react";
-
-const initialValue = {
-  firstName: "",
-  lastName: "",
-  address: "",
-  city: "",
-  state: "",
-  zip: "",
-};
+import useForm from '../hooks/useForm'
 
 // This form should be handled by a "useForm" custom hook
 // Build out the logic needed for a form custom hook (see the useForm.js file)
 // and replace the necessary stateful logic from CheckoutForm with the hook
 
 const CheckoutForm = (props) => {
-  const [showSuccessMessage, setShowSuccessMessage] = useState(false);
-  const [values, setValues] = useState(initialValue);
+  const [showSuccessMessage, values, handleChanges, handleSubmit] = useForm() 
 
-  const handleChanges = (e) => {
-    setValues({ ...values, [e.target.name]: e.target.value });
-  };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    setShowSuccessMessage(true);
-  };
 
   return (
     <>
