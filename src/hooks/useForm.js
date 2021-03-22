527b2bc8d8ee2a66c6d3433dd9b6084bbba6c025
Karla Martinez <karlamartinezmoc@gmail.com>
2021-03-21 21:33:53 -0400

    useForm changes

--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -1,1 +1,24 @@
 // write your custom hook here to control your checkout form
+import {useLocalStorage} from "./useLocalStorage"
+
+export const useForm = (initialValue) => {
+    const [values, setValues] = useLocalStorage("form-data", initialValue);
+
+    const handleChanges = e => {
+        setValues({
+            ...values,[e.target.name]: e.target.value
+        })
+    };
+
+    const handleSubmit = e => {
+        e.preventDefault(); 
+    };
+
+    const clearForm = e => {
+        e.preventDefault();
+        setValues(initialValue)
+    };
+
+    return [values, handleChanges, handleSubmit, clearForm]
+
+}
