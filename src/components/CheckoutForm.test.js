7417fd2f8f6e3e20a42a2fbb928f09f7c3beeee7
ALDO <aldo@aldos-mbp.attlocal.net>
2021-04-09 09:34:34 -0700

    MVP

--- src/components/CheckoutForm.test.js
+++ src/components/CheckoutForm.test.js
@@ -1,9 +1,32 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
 import CheckoutForm from "./CheckoutForm";
+import userEvent from "@testing-library/user-event";
 
 // Write up the two tests here and make sure they are testing what the title shows
 
-test("form header renders", () => {});
+test("form header renders", () => {
+  render(<CheckoutForm />);
 
-test("form shows success message on submit with form details", () => {});
+  const header = screen.getByText("Checkout Form");
+
+  expect(header).toBeInTheDocument();
+});
+
+test("form shows success message on submit with form details", async () => {
+  render(<CheckoutForm />);
+
+  //   const name = screen.getByLabelText("First Name:");
+
+  //   userEvent.type(name, "ALDO");
+
+  const btn = screen.getByRole("button");
+
+  userEvent.click(btn);
+
+  const submittedSuccess1 = await screen.getByText(
+    "You have ordered some plants! Woo-hoo!"
+  );
+
+  expect(submittedSuccess1).toBeVisible();
+});
