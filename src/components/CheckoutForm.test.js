188507b86c997e0bac152ffc7642a9a645ff89d8
Molly Brown <mollybrown0516@gmail.com>
2021-04-18 19:18:50 -0400

    Completed CheckoutForm.test

--- src/components/CheckoutForm.test.js
+++ src/components/CheckoutForm.test.js
@@ -1,9 +1,26 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
+import userEvent from '@testing-library/user-event';
 import CheckoutForm from "./CheckoutForm";
 
 // Write up the two tests here and make sure they are testing what the title shows
+test ("component CheckoutForm renders", () => {
+    render(<CheckoutForm/>);
 
-test("form header renders", () => {});
+});
 
-test("form shows success message on submit with form details", () => {});
+test("form header renders", () => {
+    render(<CheckoutForm />);
+    const header = screen.getByText(/Checkout Form/i);
+
+    expect(header).toBeVisible();
+    expect(header).toHaveTextContent('Checkout Form');
+});
+
+test("form shows success message on submit with form details", () => {
+    render(<CheckoutForm/>);
+    const submitButton = screen.getByRole("button", { value: /Checkout/i});
+    expect(submitButton).not.toBeDisabled();
+    
+    userEvent.click(submitButton);
+});
