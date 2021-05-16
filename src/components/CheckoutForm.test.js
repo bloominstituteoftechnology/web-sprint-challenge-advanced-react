8fb4aa0dffe7d3ce1318911ed819247b05d11dcb
SpacelessTime <73561672+SpacelessTime@users.noreply.github.com>
2021-05-16 12:12:02 -0400

    trying to get codegrade to work

--- src/components/CheckoutForm.test.js
+++ src/components/CheckoutForm.test.js
@@ -6,13 +6,15 @@ import userEvent from "@testing-library/user-event"
 // Write up the two tests here and make sure they are testing what the title shows
 
 test("form header renders", () => {
+
 render(<CheckoutForm/>)
     const header = screen.queryByText(/checkout form/i);
     expect(header).toBeInTheDocument()
 });
 
 test("form shows success message on submit with form details", () => {
-    render(<CheckoutForm/>);
+
+render(<CheckoutForm/>);
 
 const firstName = screen.getByLabelText(/first name:/i);
     userEvent.type(firstName, "Kendrick");
