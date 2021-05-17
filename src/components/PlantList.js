00afde48ec283251a32a1c49c614140ef1bdd6a1
JuanCalixto <juancalixtomorales@gmail.com>
2021-05-16 20:59:07 -0500

    render plant lists

--- src/components/PlantList.js
+++ src/components/PlantList.js
@@ -1,8 +1,11 @@
-import React, { Component } from "react";
-import axios from "axios";
+import React, { Component } from 'react';
+import axios from 'axios';
 
 export default class PlantList extends Component {
   // add state with a property called "plants" - initialize as an empty array
+  state = {
+    plants: null,
+  };
 
   // when the component mounts:
   //   - fetch data from the server endpoint - http://localhost:3333/plants
@@ -10,29 +13,31 @@ export default class PlantList extends Component {
 
   /*********  DON'T CHANGE ANYTHING IN THE RENDER FUNCTION *********/
   componentDidMount() {
-    axios.get('http://localhost:3333/plants')
-      .then(res => {
-        console.log(res);
+    axios.get('http://localhost:3333/plants').then((res) => {
+      this.setState({
+        plants: res.data,
       });
+      console.log(this.state.plants);
+    });
   }
 
   render() {
     return (
-      <main className="plant-list">
+      <main className='plant-list'>
         {this.state?.plants?.map((plant) => (
-          <div className="plant-card" key={plant.id} data-testid="plant-card">
-            <img className="plant-image" src={plant.img} alt={plant.name} />
-            <div className="plant-details">
-              <h2 className="plant-name">{plant.name}</h2>
-              <p className="plant-scientific-name">{plant.scientificName}</p>
+          <div className='plant-card' key={plant.id} data-testid='plant-card'>
+            <img className='plant-image' src={plant.img} alt={plant.name} />
+            <div className='plant-details'>
+              <h2 className='plant-name'>{plant.name}</h2>
+              <p className='plant-scientific-name'>{plant.scientificName}</p>
               <p>{plant.description}</p>
-              <div className="plant-bottom-row">
+              <div className='plant-bottom-row'>
                 <p>${plant.price}</p>
                 <p>☀️ {plant.light}</p>
                 <p>💦 {plant.watering}x/month</p>
               </div>
               <button
-                className="plant-button"
+                className='plant-button'
                 onClick={() => this.props.addToCart(plant)}
               >
                 Add to cart
