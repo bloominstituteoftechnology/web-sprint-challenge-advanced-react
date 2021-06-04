316384c244d838b89d046289dc2fb04bc7042551
Josh Levan <35366199+joshualevan@users.noreply.github.com>
2021-06-04 14:08:54 -0400

    test one papasses

--- src/components/CheckoutForm.test.js
+++ src/components/CheckoutForm.test.js
@@ -1,9 +1,16 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import CheckoutForm from "./CheckoutForm";
-
+import userEvent from '@testing-library/user-event';
 // Write up the two tests here and make sure they are testing what the title shows
 
-test("form header renders", () => {});
+test("form header renders", () => {
+    render(<CheckoutForm />);
+
+    const header = document.querySelector('form');
+
+    expect(header).toBeInTheDocument();
+    expect(header).toHaveTextContent(/checkout form/i);
+});
 
 test("form shows success message on submit with form details", () => {});
