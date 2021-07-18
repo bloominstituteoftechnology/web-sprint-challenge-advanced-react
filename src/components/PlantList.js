bd89b6430340d53e010c1e8ba424335bfd95308f
Angela Engel <amengel.ae@icloud.com>
2021-07-18 09:11:24 -0600

    Plant List is working

--- src/components/PlantList.js
+++ src/components/PlantList.js
@@ -3,6 +3,9 @@ import axios from "axios";
 
 export default class PlantList extends Component {
   // add state with a property called "plants" - initialize as an empty array
+    state = {
+      plants: []
+    }
 
   // when the component mounts:
   //   - fetch data from the server endpoint - http://localhost:3333/plants
@@ -11,7 +14,9 @@ export default class PlantList extends Component {
   componentDidMount() {
     axios.get('http://localhost:3333/plants')
       .then(res => {
-        
+        this.setState({
+          plants: res.data
+        })
       });
   }
 
