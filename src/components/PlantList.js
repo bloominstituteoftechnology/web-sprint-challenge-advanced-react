b0d4d4116e41ba9bebbf2e7ac77376e66c3c00c4
Craig Barcinas <cbarcinas@live.com>
2021-07-18 20:54:34 -0700

    custom hook useForm functioning properly

--- src/components/PlantList.js
+++ src/components/PlantList.js
@@ -3,23 +3,30 @@ import axios from "axios";
 
 export default class PlantList extends Component {
   // add state with a property called "plants" - initialize as an empty array
+  constructor() {
+    super(); //calling super in constructor gives us access to 'this'
+
+    this.state = {
+      plants: [],
+    };
+  }
 
   // when the component mounts:
   //   - fetch data from the server endpoint - http://localhost:3333/plants
   //   - set the returned plants array to this.state.plants
 
   componentDidMount() {
-    axios.get('http://localhost:3333/plants')
-      .then(res => {
-        
-      });
+    axios.get("http://localhost:3333/plants").then((res) => {
+      console.log('axios response',res);
+      this.setState({ ...this.state.plants, plants: res.data });
+    });
   }
 
   /*********  DON'T CHANGE ANYTHING IN THE RENDER FUNCTION *********/
   render() {
     return (
       <main className="plant-list">
-        {this.state.plants?.map((plant) => (
+        {this.state.plants.map((plant) => (
           <div className="plant-card" key={plant.id} data-testid="plant-card">
             <img className="plant-image" src={plant.img} alt={plant.name} />
             <div className="plant-details">
--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -1,1 +1,22 @@
 // write your custom hook here to control your checkout form
+import { useState } from "react";
+// import the initial value from it's parent component
+import { initialValue } from "../components/CheckoutForm";
+
+
+export const useForm = (initialValue) => {
+  const [showSuccessMessage, setShowSuccessMessage] = useState(false);
+  const [values, setValues] = useState(initialValue);
+
+  const handleChanges = (e) => {
+    setValues({ ...values, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setShowSuccessMessage(true);
+  };
+
+  // return all values that our form may need (similar as passing props)
+  return [values, handleSubmit, handleChanges, showSuccessMessage];
+};
