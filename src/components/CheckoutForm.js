e2687df8907f132d4867b128f36ec51e773c46ef
Alieze-Ali <71855821+Alieze-Ali@users.noreply.github.com>
2021-08-15 17:34:49 -0700

    broke some stuff

--- src/components/CheckoutForm.js
+++ src/components/CheckoutForm.js
@@ -1,6 +1,6 @@
 import { reset } from "jest-runtimenode_modules/@types/yargs";
 import React, { useState } from "react";
-import { useForm } from "../hooks/useForm";
+//import { useForm } from "../hooks/useForm";
 
 const initialValue = {
   firstName: "",
@@ -16,16 +16,16 @@ const initialValue = {
 // and replace the necessary stateful logic from CheckoutForm with the hook
 
 const CheckoutForm = (props) => {
-  const [showSuccessMessage, setShowSuccessMessage] = useState(false);
-  const [values, handleChanges] = useForm(initialValue);
+  const [showSuccessMessage, setShowSuccessMessage] = useState(false)
+  const [values, handleChanges] = useState(initialValue)
 
   const handleChanges = (e) => {
-    setValues({ ...values, [e.target.name]: e.target.value });
+    setValues({ ...values, [e.target.name]: e.target.value })
   };
 
   const handleSubmit = (e) => {
-    e.preventDefault();
-    setShowSuccessMessage(true);
+    e.preventDefault()
+    setShowSuccessMessage(true)
     // ??? cant get my clear working ???
     //reset();
   };
