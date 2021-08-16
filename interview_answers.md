530db4934527d8c53206957d4c29f91739a713bf
Alieze-Ali <71855821+Alieze-Ali@users.noreply.github.com>
2021-08-15 23:58:10 -0700

    added tests

--- interview_answers.md
+++ interview_answers.md
@@ -5,6 +5,7 @@ Be prepared to demonstrate your understanding of this week's concepts by answeri
 
 2. When does a componentWillMount function be called? What about a componentWillUpdate?
 
+
 3. Define stateful logic.
 
 4. What are the three step of creating a successful test? What is done in each phase?
--- src/components/CheckoutForm.test.js
+++ src/components/CheckoutForm.test.js
@@ -7,14 +7,43 @@ import CheckoutForm from "./CheckoutForm";
 // Write up the two tests here and make sure they are testing what the title shows
 
 test("renders without errors", () => {
-    // render(<CheckoutForm />);
-    // screen.debug;
-    // const form = screen.getByText(/checkout form/i);
-    // expect(form).toBeInTheDocument();
+    render(<CheckoutForm />);
+    //screen.debug;
+    const form = screen.getByText(/checkout form/i);
+    expect(form).toBeInTheDocument();
 
 });
 
 test("shows success message on submit with form details", () => {
+    render(<CheckoutForm />);
+    
+    const {getByLabelText} = screen;
+    const first = getByLabelText(/first name/i);
+    userEvent.type(first, 'Jason');
 
+    const {getByLabelText} = screen;
+    const last = getByLabelText(/last name/i);
+    userEvent.type(last, 'Bourne');
 
+    const {getByLabelText} = screen;
+    const address = getByLabelText(/address/i);
+    userEvent.type(address, '1234 Nottinghill Lane');
+
+    const {getByLabelText} = screen;
+    const city = getByLabelText(/city/i);
+    userEvent.type(city, 'Paris');
+
+    const {getByLabelText} = screen;
+    const state = getByLabelText(/state/i);
+    userEvent.type(state, 'France');
+
+    const {getByLabelText} = screen;
+    const zip = getByLabelText(/zip/i);
+    userEvent.type(state, '902123');
+
+    const button = screen.getByRole("button");
+    userEvent.click(button);
+
+    
+    //console.log(button)
 });
