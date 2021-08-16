0fce3a533a7ccc6aa537f6a8d29429bde56d89d3
jose-mercado <josefmercado99@gmail.com>
2021-08-15 22:55:48 -0700

    all done

--- src/components/CheckoutForm.js
+++ src/components/CheckoutForm.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import useForm from "../hooks/useForm";
 
 const initialValue = {
   firstName: "",
@@ -14,17 +15,8 @@ const initialValue = {
 // and replace the necessary stateful logic from CheckoutForm with the hook
 
 const CheckoutForm = (props) => {
-  const [showSuccessMessage, setShowSuccessMessage] = useState(false);
-  const [values, setValues] = useState(initialValue);
 
-  const handleChanges = (e) => {
-    setValues({ ...values, [e.target.name]: e.target.value });
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    setShowSuccessMessage(true);
-  };
+  const [showSuccessMessage, values, handleChanges, handleSubmit] = useForm(initialValue)
 
   return (
     <>
@@ -88,6 +80,5 @@ const CheckoutForm = (props) => {
       )}
     </>
   );
-};
-
+  };
 export default CheckoutForm;
--- src/components/CheckoutForm.test.js
+++ src/components/CheckoutForm.test.js
@@ -1,10 +1,40 @@
 import React from "react";
 import MutationObserver from 'mutationobserver-shim';
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import CheckoutForm from "./CheckoutForm";
+import userEvent from '@testing-library/user-event'
 
 // Write up the two tests here and make sure they are testing what the title shows
+    test("renders without errors", () => {
+        render(<CheckoutForm />);
+        const header = screen.getByText(/Checkout Form/i);
+        expect(header).toBeInTheDocument();
+    });
 
-test("renders without errors", () => {});
 
-test("shows success message on submit with form details", () => {});
+test("shows success message on submit with form details", () => {
+    render(<CheckoutForm />)
+    const firstName = screen.getByLabelText(/First Name/i)
+    const lastName = screen.getByLabelText(/Last Name/i)
+    const address = screen.getByLabelText(/Address/i)
+    const city = screen.getByLabelText(/City/i)
+    const state = screen.getByLabelText(/State/i)
+    const zip = screen.getByLabelText(/Zip/i)
+    const submitButton = document.querySelector('button')
+
+    userEvent.type(firstName, 'Jose')
+    userEvent.type(lastName, 'Mercado')
+    userEvent.type(address, '123 seasame st')
+    userEvent.type(city, 'stockton')
+    userEvent.type(state, 'CA')
+    userEvent.type(zip, '95207')
+    userEvent.click(submitButton)
+
+    const successMessage = document.querySelector("[data-testid = 'successMessage']")
+
+    expect(successMessage).toHaveTextContent(/Jose Mercado/)
+    expect(successMessage).toHaveTextContent(/123 seasame st/)
+    expect(successMessage).toHaveTextContent(/stockton/)
+    expect(successMessage).toHaveTextContent(/CA/)
+    expect(successMessage).toHaveTextContent(/95207/)
+});
