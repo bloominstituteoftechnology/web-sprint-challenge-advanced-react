6a024bb4ef09f292ac79d668cf21d1ac1a1b0433
Purefallen11 <53922216+Purefallen11@users.noreply.github.com>
2021-08-16 01:00:44 -0500

    i believe im done

--- src/components/CheckoutForm.test.js
+++ src/components/CheckoutForm.test.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
+import MutationObserver from 'mutationobserver-shim';
 import userEvent from '@testing-library/user-event'
 import CheckoutForm from "./CheckoutForm";
 
