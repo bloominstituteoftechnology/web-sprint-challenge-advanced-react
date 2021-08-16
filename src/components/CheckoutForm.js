0464a7df0e458cd2386bf6bb81e207b2c6236c95
Alieze-Ali <71855821+Alieze-Ali@users.noreply.github.com>
2021-08-15 18:02:48 -0700

    added custom hook

--- src/components/CheckoutForm.js
+++ src/components/CheckoutForm.js
@@ -1,6 +1,6 @@
 //import { reset } from "jest-runtimenode_modules@types/yargs";
 import React, { useState } from "react";
-//import { useForm } from "../hooks/useForm";
+import  useForm  from "../hooks/useForm";
 
 const initialValue = {
   firstName: "",
@@ -17,11 +17,11 @@ const initialValue = {
 
 const CheckoutForm = (props) => {
   const [showSuccessMessage, setShowSuccessMessage] = useState(false);
-  const [values, setValues] = useState(initialValue);
+  const [values, handleChanges] = useForm(initialValue);
 
-  const handleChanges = (e) => {
-    setValues({ ...values, [e.target.name]: e.target.value });
-  };
+  // const handleChanges = (e) => {
+  //   setValues({ ...values, [e.target.name]: e.target.value });
+  // };
 
   const handleSubmit = (e) => {
     e.preventDefault();
