9e565a402029eb87af8d9ef9afdcc46995c167c2
Purefallen11 <53922216+Purefallen11@users.noreply.github.com>
2021-08-16 00:45:26 -0500

    writing test

--- src/components/CheckoutForm.test.js
+++ src/components/CheckoutForm.test.js
@@ -1,10 +1,35 @@
 import React from "react";
-import MutationObserver from 'mutationobserver-shim';
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
+import userEvent from '@testing-library/user-event'
 import CheckoutForm from "./CheckoutForm";
 
 // Write up the two tests here and make sure they are testing what the title shows
 
-test("renders without errors", () => {});
+test("renders without errors", () => {
+	render(<CheckoutForm />);
 
-test("shows success message on submit with form details", () => {});
+	const checkoutForm = screen.getById(/checkout form/i)
+
+	expect(checkoutForm).toBeInTheDocument()
+	expect(checkoutForm).toHaveTextContent()
+	expect(checkoutForm).toBeTruthy()
+
+
+});
+
+test("shows success message on submit with form details", () => {
+
+	render(<CheckoutForm />)
+
+	const checkoutForm = screen.getById(/checkout form/i)
+
+	const firstname = screen.getByLabelText(/First Name:/i)
+	const lastname = screen.getByLabelText(/Last Name:/i)
+	const address = screen.getByLabelText(/Address:/i)
+	const city = screen.getByLabelText(/City:/i)
+	const state = screen.getByLabelText(/State/i)
+	const zip = screen.getByLabelText(/Zip:/i)
+	const checkout = screen.queryByRole('button')
+
+	
+});
--- src/components/PlantList.js
+++ src/components/PlantList.js
@@ -1,4 +1,4 @@
-import React, { Component, useState } from "react";
+import React, { Component } from "react";
 import axios from "axios";
 
 export default class PlantList extends Component {
