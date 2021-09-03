bf718d5f921230dfcd5a7c2cde9b270ed4e967e9
ModupeD <bimisoladaniel@Bimisolas-MacBook-Air.local[credential]>
2021-09-03 15:20:34 +0100

    Your helpful commit message here

--- src/components/PlantList.js
+++ src/components/PlantList.js
@@ -3,16 +3,19 @@ import axios from "axios";
 
 export default class PlantList extends Component {
   // add state with a property called "plants" - initialize as an empty array
-
+  state = {
+    plants: [],
+  };
   // when the component mounts:
   //   - fetch data from the server endpoint - http://localhost:3333/plants
   //   - set the returned plants array to this.state.plants
 
   componentDidMount() {
-    axios.get('http://localhost:3333/plants')
-      .then(res => {
-        
+    axios.get("http://localhost:3333/plants").then((res) => {
+      this.setState({
+        plants: res.data,
       });
+    });
   }
 
   /*********  DON'T CHANGE ANYTHING IN THE RENDER FUNCTION *********/
--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -1,1 +1,8 @@
 // write your custom hook here to control your checkout form
+import { CheckoutForm } from "./CheckoutForm";
+
+export const useForm = (initialVaue) => {
+  const [value, setValue] = CheckoutForm("form", initialVaue);
+
+  return [value, setValue];
+};
