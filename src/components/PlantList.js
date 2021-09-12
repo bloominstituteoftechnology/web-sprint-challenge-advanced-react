86f931f741f1557aaca7272428b52ab72420045d
vida <ofo28@aol.com>
2021-09-11 20:40:21 -0400

    components updated

--- src/components/PlantList.js
+++ src/components/PlantList.js
@@ -1,9 +1,15 @@
 import React, { Component } from "react";
+
 import axios from "axios";
 
 export default class PlantList extends Component {
   // add state with a property called "plants" - initialize as an empty array
-
+constructor(){
+  super();
+  this.state = {
+    plants:"",
+  }
+}
   // when the component mounts:
   //   - fetch data from the server endpoint - http://localhost:3333/plants
   //   - set the returned plants array to this.state.plants
@@ -11,7 +17,7 @@ export default class PlantList extends Component {
   componentDidMount() {
     axios.get('http://localhost:3333/plants')
       .then(res => {
-        
+        return (this.state.plants)
       });
   }
 
@@ -19,7 +25,7 @@ export default class PlantList extends Component {
   render() {
     return (
       <main className="plant-list">
-        {this.state.plants?.map((plant) => (
+        {this.state.plants.map((plant) => (
           <div className="plant-card" key={plant.id} data-testid="plant-card">
             <img className="plant-image" src={plant.img} alt={plant.name} />
             <div className="plant-details">
--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -1,1 +1,13 @@
 // write your custom hook here to control your checkout form
+import checkoutForm from "../components/CheckoutForm"
+
+export const useForm = (param) => {
+    const [form, setForm] = checkoutForm(param, false)
+    const [values, setValues] = useForm(param);
+    
+    const handleChanges = (e) => {
+        setValues({ ...values, [e.target.name]: e.target.value });
+      };
+
+    return([form, handleChanges])
+}
