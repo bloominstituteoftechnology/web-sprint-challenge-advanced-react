984a0dc640321ca863d40397783c306af63d3b07
Aaron <aarongabriel147@gmail.com>
2021-09-12 15:19:46 -0700

    testing

--- src/components/CheckoutForm.test.js
+++ src/components/CheckoutForm.test.js
@@ -1,11 +1,9 @@
-// import React from "react";
-// import MutationObserver from 'mutationobserver-shim';
-// import { render } from "@testing-library/react";
-// import CheckoutForm from "./CheckoutForm";
-
 import React from "react";
-import { render, screen } from "@testing-library/react";
+import MutationObserver from 'mutationobserver-shim';
+import { screen, render ,waitFor} from "@testing-library/react";
 import CheckoutForm from "./CheckoutForm";
+import userEvent from '@testing-library/user-event';
+
 
 // Write up the two tests here and make sure they are testing what the title shows
 
@@ -17,9 +15,53 @@ test("form header renders", () => {
     expect(headerElement).toBeInTheDocument();
 });
 
-test("form shows success message on submit with form details", () => {
-    render (<CheckoutForm />);
-    screen.debug()
-    const successMSG = screen.queryByText(/You have ordered some plants! Woo-hoo!/i)
-    // expect(successMSG).toBeInTheDocument();
+
+test("shows success message on submit with form details",  () => {
+    render(<CheckoutForm />);
+
+    const button = screen.getByRole("button");
+    userEvent.click(button);
+
+    // await waitFor(()=>{
+        const successMessages = screen.getByText(/You have ordered some plants! Woo-hoo!/i);
+        expect(successMessages).toBeInTheDocument();
+    // })
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -1,7 +1,4 @@
-// write your custom hook here to control your checkout form
 import { useState } from 'react';
-
-
 
 
 const useForm = (initialValue) => {
@@ -9,7 +6,7 @@ const useForm = (initialValue) => {
     const [booleanValue, setBooleanValue] = useState(false) // I could have added a bool key to the initialValue object most likely then I would have only needed 1 state/object.
 
     const handleChanges = (e) => {
-        setValues({ ...values, [e.target.name]: e.target.value });
+        setValues({ ...values, [e.target.name] : e.target.value });
       };
     
       const handleSubmit = (e) => {
