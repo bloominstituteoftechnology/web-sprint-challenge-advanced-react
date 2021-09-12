7f4ed98c10b8e77a1030980fb4e1eb35448c6b80
nicole pizzano <pizzano.nicole@gmail.com>
2021-09-12 10:40:36 -0400

    hook save

--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -1,1 +1,21 @@
 // write your custom hook here to control your checkout form
+import { useState} from 'react';
+
+export const useForm = (initialState) => {
+    const [value, setValue] = useState(initialState);
+    const handleChange = (e) => {
+        setValue({
+            ...value,
+            [e.target.name]: e.target.value
+        });
+    }
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(value);
+    }
+    return {
+        value,
+        handleChange,
+        handleSubmit
+    }
+}
