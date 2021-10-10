2b94ac159478a5d8d4a62c0f2ccedcd09916157d
Anthony Rodriguez <antcreatesmagic@gmail.com>
2021-10-10 14:52:58 -0500

    fetch plant data

--- src/components/PlantList.js
+++ src/components/PlantList.js
@@ -3,12 +3,27 @@ import axios from "axios";
 
 export default class PlantList extends Component {
   // add state with a property called "plants" - initialize as an empty array
-  state = {
-    plants:[]
+constructor(){
+  super()
+  this.state = {
+    plants: []
   }
+}
   // when the component mounts:
   //   - fetch data from the server endpoint - http://localhost:3333/plants
   //   - set the returned plants array to this.state.plants
+
+  componentDidMount(){
+    axios.get('http://localhost:3333/plants')
+    .then( res=> {
+      this.setState({plants: res.data})
+    })
+    .catch( err => {
+      console.log(err)
+    }
+      
+    )
+  }
 
   /*********  DON'T CHANGE ANYTHING IN THE RENDER FUNCTION *********/
   render() {
