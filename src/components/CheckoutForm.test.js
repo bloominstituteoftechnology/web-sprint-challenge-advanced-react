fc6a91d35a2974177e8f17b1b998aaacf6647e2c
WaylayOvercast <timelyovercast@gmail.com>
2021-10-29 16:02:49 -0500

    finished testing

--- src/components/CheckoutForm.test.js
+++ src/components/CheckoutForm.test.js
@@ -1,10 +1,51 @@
 import React from "react";
 import MutationObserver from 'mutationobserver-shim';
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import CheckoutForm from "./CheckoutForm";
+import userEvent from "@testing-library/user-event";
 
 // Write up the two tests here and make sure they are testing what the title shows
 
-test("renders without errors", () => {});
+test("renders without errors", () => {
+    render(<CheckoutForm/>)
+    
+});
+    
 
-test("shows success message on submit with form details", () => {});
+test("shows success message on submit with form details", () => {
+    render(<CheckoutForm/>)
+    // grabbing elements 
+    
+    const firstInput = screen.getByLabelText('First Name:');
+    const lastInput = screen.getByLabelText('Last Name:');
+    const addressInput = screen.getByLabelText('Address:');
+    const cityInput = screen.getByLabelText('City:');
+    const stateInput = screen.getByLabelText('State:');
+    const zipInput = screen.getByLabelText('Zip:');
+    const submit = screen.getByRole('button');
+
+    // typing into inputs
+    userEvent.type(firstInput, 'testfirst');
+    userEvent.type(lastInput, 'testlast');
+    userEvent.type(addressInput, 'testadd');
+    userEvent.type(cityInput, 'testcity');
+    userEvent.type(stateInput, 'teststate');
+    userEvent.type(zipInput, 'testzip');
+    userEvent.click(submit);
+
+    // grabbing exact form response
+    const succage = screen.getByText(/You have ordered some plants!/);
+    const nameOut = screen.getByText(/testfirst/);
+    const addressOut = screen.getByText('testadd')
+    const cityOut = screen.getByText(/testcity/)
+    const zipOut = screen.getByText(/testzip/)
+    
+    
+    // expect form to have exact changes
+    expect(succage).toBeInTheDocument()
+     expect(nameOut).toBeInTheDocument('testfirs')
+     expect(addressOut).toBeInTheDocument('testadd')
+     expect(cityOut).toBeInTheDocument('testcity')
+     expect(zipOut).toBeInTheDocument('testzip')
+    
+});
--- src/components/PlantList.js
+++ src/components/PlantList.js
@@ -9,7 +9,13 @@ export default class PlantList extends Component {
   // when the component mounts:
   //   - fetch data from the server endpoint - http://localhost:3333/plants
   //   - set the returned plants array to this.state.plants
+  componentDidMount(){
 
+    axios.get('http://localhost:3333/plants')
+    .then(res => this.setState({...this.state, plants: res.data}))
+    .catch(err => console.error(err))
+
+  }
   /*********  DON'T CHANGE ANYTHING IN THE RENDER FUNCTION *********/
   render() {
     return (
