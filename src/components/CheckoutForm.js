f597cb622227656d260bf3f6de7d6b885575ea98
Shivneel-Prasad <Shivam201020@gmail.com>
2021-12-03 04:31:52 -0800

    updated checkoutforms and useform

--- src/components/CheckoutForm.js
+++ src/components/CheckoutForm.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import useForm from "../hooks/useForm";
 
 const initialValue = {
   firstName: "",
@@ -15,7 +16,7 @@ const initialValue = {
 
 const CheckoutForm = (props) => {
   const [showSuccessMessage, setShowSuccessMessage] = useState(false);
-  const [values, setValues] = useState(initialValue);
+  const [values, setValues] = useForm(initialValue);
 
   const handleChanges = (e) => {
     setValues({ ...values, [e.target.name]: e.target.value });
--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -1,1 +1,12 @@
 // write your custom hook here to control your checkout form
+import React, { useState } from 'react'
+
+const useForm = (initialValues) => {
+    const [values, setValues] = useState(initialValues)
+
+    return (
+        [values, setValues]
+    )
+}
+
+export default useForm;
