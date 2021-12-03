b425349f097c28a74cf9137466e86762144fe5e0
Ogden-R <rebeccaogden97@gmail.com>
2021-12-03 17:54:13 -0600

    fixed the broken tests, now running great, all tests passing.

--- src/components/CheckoutForm.test.js
+++ src/components/CheckoutForm.test.js
@@ -1,6 +1,7 @@
 import React from "react";
 import MutationObserver from 'mutationobserver-shim';
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
+import userEvent from '@testing-library/user-event';
 import CheckoutForm from "./CheckoutForm";
 
 
@@ -11,27 +12,33 @@ test("renders without errors", () => {
 
 });
 
-test("shows success message on submit with form details", async () => {
+test("shows success message on submit with form details", () => {
     render(<CheckoutForm />);
   
+    const firstName = 'Becca';
+    const lastName = 'Ogden';
+    const address = '5520 W Monroe Concord Rd';
+    const city = 'West Milton';
+    const state = 'Ohio';
+    const zip = '45383';
+
     const firstNameInput = screen.getByLabelText(/First Name:/i);
-    userEvent.type(firstNameInput, "first");
+    userEvent.type(firstNameInput, firstName);
     const lastNameInput = screen.getByLabelText(/Last Name:/i);
-    userEvent.type(lastNameInput, "last");
+    userEvent.type(lastNameInput, lastName);
     const addressInput = screen.getByLabelText(/Address:/i);
-    userEvent.type(addressInput, "address");
+    userEvent.type(addressInput, address);
     const cityInput = screen.getByLabelText(/City:/i);
-    userEvent.type(cityInput, "city");
+    userEvent.type(cityInput, city);
     const stateInput = screen.getByLabelText(/State:/i);
-    userEvent.type(stateInput, "state");
+    userEvent.type(stateInput, state);
     const zipInput = screen.getByLabelText(/Zip:/i);
-    userEvent.type(zipInput, "12345");
+    userEvent.type(zipInput, zip);
     
-    const submitButton = screen.getByRole("button");
+    const submitButton = screen.getByRole('button');
     userEvent.click(submitButton);
   
-    await waitFor(() => {
-      const successMessage = screen.getByTestId(/successMessage/i);
-      expect(successMessage).toBeInTheDocument();
-    });
+    const successMessage = screen.getByTestId(/successMessage/i);
+    expect(successMessage).toBeInTheDocument();
+    expect(successMessage).toBeTruthy();
   });
