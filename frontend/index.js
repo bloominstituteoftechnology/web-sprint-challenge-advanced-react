d380d1d3057f3bcafafca86e1de4421adc803555
Ladrillo <gabriel@cabrejas.net>
2022-05-03 19:22:49 +0200

    "upgrade deps & lockfile"

--- frontend/index.js
+++ frontend/index.js
@@ -11,16 +11,14 @@ const root = createRoot(container)
 
 root.render(
   <BrowserRouter>
-    <React.StrictMode>
-      <h1>Welcome to the GRID</h1>
-      <nav>
-        <NavLink to="/">Functional</NavLink>
-        <NavLink to="/class-based">Class-Based</NavLink>
-      </nav>
-      <Routes>
-        <Route path="/" element={<AppFunctional className="functional" />} />
-        <Route path="class-based" element={<AppClass className="class-based" />} />
-      </Routes>
-    </React.StrictMode>
+    <h1>Welcome to the GRID</h1>
+    <nav>
+      <NavLink to="/">Functional</NavLink>
+      <NavLink to="/class-based">Class-Based</NavLink>
+    </nav>
+    <Routes>
+      <Route path="/" element={<AppFunctional className="functional" />} />
+      <Route path="class-based" element={<AppClass className="class-based" />} />
+    </Routes>
   </BrowserRouter>
 )
