87c3536651113324b31b4621ec9e115097035d62
Ladrillo <gabriel@cabrejas.net>
2022-06-07 17:10:45 +0200

    comment

--- frontend/components/AppClass.js
+++ frontend/components/AppClass.js
@@ -14,6 +14,9 @@ const initialState = {
 }
 
 export default class AppClass extends React.Component {
+  // THE FOLLOWING HELPERS ARE JUST RECOMMENDATIONS.
+  // You can delete them and build your own logic from scratch.
+
   getXY = () => {
     // It it not necessary to have a state to track the coordinates.
     // It's enough to know what index the "B" is at, to be able to calculate them.
--- frontend/components/AppFunctional.js
+++ frontend/components/AppFunctional.js
@@ -7,6 +7,9 @@ const initialSteps = 0
 const initialIndex = 4 // the index the "B" is at
 
 export default function AppFunctional(props) {
+  // THE FOLLOWING HELPERS ARE JUST RECOMMENDATIONS.
+  // You can delete them and build your own logic from scratch.
+
   function getXY() {
     // It it not necessary to have a state to track the coordinates.
     // It's enough to know what index the "B" is at, to be able to calculate them.
